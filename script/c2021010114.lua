c0b394257d7ac36767770bfb4106340848af47f7
Almatik <almatiktk@gmail.com>
2021-04-14 09:31:07 +0600

    Add files via upload

--- script/c2021010114.lua
+++ script/c2021010114.lua
@@ -6,18 +6,18 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--trigger
+	--damage
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_DAMAGE)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetCondition(s.damcon)
-	e2:SetTarget(s.damtg)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCode(EVENT_BATTLE_DAMAGE)
+	e2:SetCondition(s.damcon1)
 	e2:SetOperation(s.damop)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCode(EVENT_DAMAGE)
+	e3:SetCondition(s.damcon2)
 	c:RegisterEffect(e3)
 	--atk/def
 	local e4=Effect.CreateEffect(c)
@@ -26,51 +26,23 @@ function s.initial_effect(c)
 	e4:SetRange(LOCATION_FZONE)
 	e4:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
 	e4:SetTarget(aux.NOT(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_FIRE)))
-	e4:SetValue(-300)
+	e4:SetValue(-400)
 	c:RegisterEffect(e4)
 	local e5=e4:Clone()
 	e5:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e5)
 end
-function s.damcon(e,tp,eg,ep,ev,re,r,rp)
-	return #eg==1
+function s.damcon1(e,tp,eg,ep,ev,re,r,rp)
+	return eg:GetFirst():IsAttribute(ATTRIBUTE_FIRE)
 end
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_FIRE)
-end
-function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local tc=eg:GetFirst()
-	if chk==0 then return true end
-	local p=0
-	if not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil) then
-		p=p+1
-	end
-	if not Duel.IsExistingMatchingCard(s.cfilter,tp,0,LOCATION_MZONE,1,nil) then
-		p=p+2
-	end
-	Duel.SetTargetCard(eg)
-	if p==1 then
-		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,tc:GetAttack()/2)
-	elseif p==2 then
-		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,tc:GetAttack()/2)
-	elseif p==3 then
-		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,PLAYER_ALL,tc:GetAttack()/2)
-	end
+function s.damcon2(e,tp,eg,ep,ev,re,r,rp)
+	return r&REASON_BATTLE==0 and re and re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsAttribute(ATTRIBUTE_FIRE)
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
-	if not e:GetHandler():IsRelateToEffect(e) or not tc or not tc:IsRelateToEffect(e) then return end
-	local p=0
-	if not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil) then
-		p=p+1
-	end
-	if not Duel.IsExistingMatchingCard(s.cfilter,tp,0,LOCATION_MZONE,1,nil) then
-		p=p+2
-	end
-	if p==1 or p==3 then
-		Duel.Damage(tp,tc:GetAttack()/2,REASON_EFFECT)
-	end
-	if p==2 or p==3 then
-		Duel.Damage(1-tp,tc:GetAttack()/2,REASON_EFFECT)
+	Duel.Hint(HINT_CARD,0,id)
+	if ep~=tp then
+		Duel.Damage(1-tp,400,REASON_EFFECT)
+	else
+		Duel.Damage(tp,400,REASON_EFFECT)
 	end
 end
