626acd49d748c85dbe957abf2b7ab18bc009ec78
Almatik <almatiktk@gmail.com>
2021-04-15 12:44:46 +0600

    Add files via upload

--- script/c2021010117.lua
+++ script/c2021010117.lua
@@ -43,5 +43,18 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,lv)
 		if #sg>0 then Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP) end
+		--Cannot special summon for rest of turn, except FIRE monsters
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+		e1:SetTargetRange(1,0)
+		e1:SetTarget(s.splimit)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+		aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,1),nil)
 	end
+end
+function s.splimit(e,c,sump,sumtype,sumpos,targetp)
+	return not c:IsAttribute(ATTRIBUTE_FIRE)
 end
