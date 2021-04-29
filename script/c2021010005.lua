7b231a3c906f53faa5d188789fbfd78e9a73e797
Almatik <almatiktk@gmail.com>
2021-04-29 18:48:29 +0600

    Update c2021010005.lua

--- script/c2021010005.lua
+++ script/c2021010005.lua
@@ -1,4 +1,7 @@
 --Junk Titanic Warrior
-function c2021010005.initial_effect(c)
-	
+local s,id=GetID()
+function s.initial_effect(c)
+	--synchro summon
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	c:EnableReviveLimit()
 end
