c07a404c44e786baf0667b7519e1c7f8730d12fa
Almatik <almatiktk@gmail.com>
2021-04-29 19:22:46 +0600

    Update c2021010005.lua

--- script/c2021010005.lua
+++ script/c2021010005.lua
@@ -30,8 +30,11 @@ end
 function s.drcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
+function s.drfilter(c)
+	return c:IsSetCard(0x66)
+end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ct=e:GetHandler():GetMaterial():FilterCount(aux.Filter(Card.IsSetCard,0x66),nil)
+	local ct=e:GetHandler():GetMaterial():FilterCount(s.drfilter,nil)
 	if chk==0 then return ct>0 and Duel.IsPlayerCanDraw(tp,ct) end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(ct)
