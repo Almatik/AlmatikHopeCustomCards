1d0cc301d5efec2811a609675be664270a1c3fa1
Almatik <almatiktk@gmail.com>
2021-04-29 16:42:19 +0600

    Add files via upload

--- script/c2021010003.lua
+++ script/c2021010003.lua
@@ -39,7 +39,7 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)~=0 then
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)~=0 then
 		local c=e:GetHandler()
 		--Tuner
 		local e1=Effect.CreateEffect(c)
