4bb054edff29376f4e178c8ca569fac0e02a99f8
Almatik <almatiktk@gmail.com>
2021-04-29 19:32:00 +0600

    Update c2021010005.lua

--- script/c2021010005.lua
+++ script/c2021010005.lua
@@ -62,7 +62,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) 
 		if c:IsFaceup() and c:IsRelateToEffect(e) then
-			local lv=tc:GetFirst():GetLevel()
+			local lv=tc:GetLevel()
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_LEVEL)
