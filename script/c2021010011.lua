c8eedc42de3ddc7068338b5b2986975056944e78
Almatik <almatiktk@gmail.com>
2021-05-01 19:43:46 +0600

    Update c2021010011.lua

--- script/c2021010011.lua
+++ script/c2021010011.lua
@@ -1,5 +1,8 @@
 --Firewall Fatal Dragon
 local s,id=GetID()
 function s.initial_effect(c)
+	--fusion material
+	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,RACE_CYBERSE),2)
+	c:EnableReviveLimit()
 	
 end
