097ced12d4eb310622cccf326cf2b11ad6a54bc7
Almatik <almatiktk@gmail.com>
2021-05-04 21:37:38 +0600

    Update c2021010014.lua

--- script/c2021010014.lua
+++ script/c2021010014.lua
@@ -37,21 +37,6 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		if #g>0 then
 			Duel.SendtoHand(g,nil,REASON_EFFECT)
 			Duel.ConfirmCards(1-tp,g)
-			if Duel.IsExistingTarget(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) and Duel.SelectYesNo(0,aux.Stringid(id,0)) then
-				local stc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_HAND,0,1,1,nil)
-				local dtc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil,stc:GetCode())
-				Duel.Destroy(dtc,REASON_EFFECT)
-				Duel.SpecialSummon(stc,SUMMON_TYPE_RITUAL,tp,tp,false,false,POS_FACEUP)
-			end
 		end
 	end
-end
-function s.spfilter(c,e,tp)
-	local lv=c:GetLevel()
-	local g=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_MZONE,0,nil,e:GetCode())
-	return lv>0 and c:IsRace(RACE_DRAGON) and c:IsType(TYPE_RITUAL) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,false)
-		and g:GetSum(Card.GetLevel)
-end
-function s.rmfilter(c,code)
-	return c:GetLevel()>0 and c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_DRAGON) and c:IsDestructable() and c:GetCode()~=code
 end
