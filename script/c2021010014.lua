7c9407a57b6e77f7dc2f2c964433d25d85638da6
Almatik <almatiktk@gmail.com>
2021-05-04 23:54:17 +0600

    Update c2021010014.lua

--- script/c2021010014.lua
+++ script/c2021010014.lua
@@ -1,6 +1,7 @@
 --Borrel Reform
 local s,id=GetID()
 function s.initial_effect(c)
+	local rparams={filter=aux.FilterBoolFunction(Card.IsRace,RACE_DRAGON),lvtype=RITPROC_GREATER,extraop=s.extraop,matfilter=s.mfilter}
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SEARCH)
@@ -9,12 +10,8 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetOperation(s.operation(Ritual.Target(rparams),Ritual.Operation(rparams)))
 	c:RegisterEffect(e1)
-	--Ritual
-	local e2=Ritual.CreateSummonEff({handler=c,extraop=s.extraop,lvtype=RITPROC_GREATER,location=LOCATION_HAND,matfilter=s.mfilter})
-	e2:SetCountLimit(1,id+1)
-	c:RegisterEffect(e2)
 end
 s.listed_series={0x102}
 function s.ddfilter(c,tp)
@@ -33,20 +30,35 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-		if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)~=0 then
-			Duel.ConfirmCards(1-tp,g)
+function s.operation(rittg,ritop)
+	return function(e,tp,eg,ep,ev,re,r,rp)
+		local tc=Duel.GetFirstTarget()
+		if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+			if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)~=0 then
+				Duel.ConfirmCards(1-tp,g)
+				local rit=rittg(e,tp,eg,ep,ev,re,r,rp,0)
+				if rit then
+					local sel={}
+					table.insert(sel,aux.Stringid(id,1))
+					if rit then table.insert(sel,aux.Stringid(id,2)) end
+					local res=Duel.SelectOption(tp,false,table.unpack(sel))
+					if res==0 then return end
+					Duel.BreakEffect()
+					if res==1 then
+						Duel.Hint(HINT_OPSELECTED,tp,aux.Stringid(id,2))
+						ritop(e,tp,eg,ep,ev,re,r,rp)
+					end
+				end
+			end
 		end
 	end
+end
+function s.mfilter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_DRAGON)
 end
 function s.extraop(e,tc,tp,sg)
 	Duel.Destroy(sg,REASON_EFFECT+REASON_MATERIAL)
 	sg:Clear()
-end
-function s.mfilter(c)
-	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_DRAGON)
 end
