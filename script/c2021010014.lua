0e708083e749ac757f9dfd6682ca208b9f612656
Almatik <almatiktk@gmail.com>
2021-05-05 00:10:21 +0600

    Update c2021010014.lua

--- script/c2021010014.lua
+++ script/c2021010014.lua
@@ -59,5 +59,5 @@ function s.mfilter(c)
 	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_DRAGON)
 end
 function s.extraop(mg,e,tp,eg,ep,ev,re,r,rp)
-	Duel.SendtoDeck(mg,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
+	Duel.Destroy(mg,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
 end
