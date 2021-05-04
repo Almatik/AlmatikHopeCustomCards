36e3a15efa2e8ae8ffdbbd4555365780264b1f31
Almatik <almatiktk@gmail.com>
2021-05-04 22:40:55 +0600

    Update c2021010014.lua

--- script/c2021010014.lua
+++ script/c2021010014.lua
@@ -7,28 +7,37 @@ function s.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id) 
-	e1:SetCondition(s.condition)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x102}
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local sp=Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
-	local desg=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_DECK,0,c)
-	return sp and desg:CheckWithSumGreater(Card.GetLevel,sp:GetLevel()-1,1,99)
+function s.ddfilter(c,tp)
+	local code=c:GetCode()
+	return c:IsFaceup() and c:IsSetCard(0x102)
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,code)
+end
+function s.thfilter(c,code)
+	return c:IsSetCard(0x102) and c:IsType(TYPE_MONSTER) and not c:IsCode(code) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.ddfilter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.ddfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,s.ddfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tr=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-	local td=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_DECK,0,nil)
-	local dg=td:SelectWithSumGreater(tp,Card.GetLevel,tr:GetLevel()-1,1,99)
-	if Duel.Destroy(dg,REASON_EFFECT+REASON_MATERIAL)~=0 then
-		Duel.SpecialSummon(tr,SUMMON_TYPE_RITUAL,tp,tp,false,false,POS_FACEUP)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+
+		end
 	end
-end
-function s.spfilter(c,e,tp)
-	return c:IsRace(RACE_DRAGON) and c:IsType(TYPE_RITUAL) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,false)
-end
-function s.desfilter(c)
-	return c:GetLevel()>0 and c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_DRAGON) and c:IsDestructable()
 end
