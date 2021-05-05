a1a400750bf0e75e36672d178b195c67e9145589
Almatik <almatiktk@gmail.com>
2021-05-05 21:43:04 +0600

    Update c2021010016.lua

--- script/c2021010016.lua
+++ script/c2021010016.lua
@@ -1,5 +1,74 @@
 --Topologic Warrior
 local s,id=GetID()
 function s.initial_effect(c)
-	
+	--synchro summon
+	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK),1,1,Synchro.NonTuner(nil),1,99)
+	c:EnableReviveLimit()
+	--cannot be destroyed by battle
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetCondition(s.indcon)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetTargetRange(s.indfilter,0)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EFFECT_IMMUNE_EFFECT)
+	e3:SetValue(s.efilter)
+	c:RegisterEffect(e3)
+	local e4=e1:Clone()
+	e4:SetCode(EFFECT_IMMUNE_EFFECT)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetTargetRange(s.indfilter,0)
+	e4:SetValue(s.efilter)
+	c:RegisterEffect(e4)
+	--Move itself to 1 of your unused MMZ
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,0))
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e5:SetCountLimit(1)
+	e5:SetCost(s.seqcost)
+	e5:SetTarget(s.seqtg)
+	e5:SetOperation(s.seqop)
+	c:RegisterEffect(e5)
+
+end
+function s.indcon(e)
+	return e:GetHandler():IsLinked()
+end
+function s.indfilter(e,re)
+	local c=e:GetHandler()
+	local rc=re:GetHandler()
+	return rc:IsFaceup() and rc:IsLinkMonster()
+		and rc:GetLinkedGroup():IsContains(c)
+end
+function s.efilter(e,re)
+	return e:GetOwnerPlayer()~=re:GetOwnerPlayer()
+end
+
+	--Move itself to 1 of your unused MMZ
+function s.seqcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.CheckLPCost(tp,1500) and c:IsControler(1-tp) end
+	Duel.PayLPCost(tp,1500)
+end
+function s.seqtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
+	local seq=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0)
+	Duel.Hint(HINT_ZONE,tp,seq)
+	e:SetLabel(math.log(seq,2))
+end
+function s.seqop(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local seq=e:GetLabel()
+	if not c:IsRelateToEffect(e) or c:IsControler(1-tp) or not Duel.CheckLocation(tp,LOCATION_MZONE,seq) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
+	Duel.MoveSequence(c,seq)
 end
