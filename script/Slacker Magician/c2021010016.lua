01f01cfe2c2e985694295f45fa0cc8dba17bac0c
Almatik <almatiktk@gmail.com>
2021-05-06 14:12:53 +0600

    Add files via upload

--- script/Slacker Magician/c2021010016.lua
+++ script/Slacker Magician/c2021010016.lua
@@ -21,8 +21,10 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetProperty(EFFECT_FLAG_BOTH_SIDE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
+	e3:SetCost(s.seqcost)
 	e2:SetTarget(s.seqtg)
 	e2:SetOperation(s.seqop)
 	c:RegisterEffect(e2)
@@ -35,7 +37,12 @@ function s.efilter(e,re)
 	return e:GetOwnerPlayer()~=re:GetOwnerPlayer()
 end
 
-	--Activation legality
+
+function s.seqcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1500) end
+	Duel.PayLPCost(tp,1500)
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+end	--Activation legality
 function s.seqtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
