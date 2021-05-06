c14c46084399859b74151c18eb22bb432ef63841
Almatik <almatiktk@gmail.com>
2021-05-06 18:42:57 +0600

    Update c2021010016.lua

--- script/Slacker Magician/c2021010016.lua
+++ script/Slacker Magician/c2021010016.lua
@@ -54,19 +54,23 @@ function s.seqcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if tp~=e:GetHandlerPlayer() then Duel.PayLPCost(tp,1500) end
 end
 function s.seqtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	if chk==0 then
+		if tp~=e:GetHandlerPlayer() then
+			return Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
+		else
+			return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		end
+	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
 end
 function s.seqop(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local seq=e:GetLabel()
-	if c:IsRelateToEffect(e) then
-		if tp==e:GetHandlerPlayer then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
-			Duel.MoveSequence(c,math.log(Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0),2))
-		else
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
-			Duel.MoveSequence(c,math.log(Duel.SelectDisableField(tp,1,0,LOCATION_MZONE,0),2))
-		end
+	if c:IsRelateToEffect(e) and tp==e:GetHandlerPlayer() then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
+		Duel.MoveSequence(c,math.log(Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0),2))
+	elseif c:IsRelateToEffect(e) and tp~=e:GetHandlerPlayer then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
+		Duel.MoveSequence(c,math.log(Duel.SelectDisableField(tp,1,0,LOCATION_MZONE,0),2))
 	end
 end
