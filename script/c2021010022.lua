ed91c425943ea02b979df3a81f80c57c46434193
Almatik <almatiktk@gmail.com>
2021-05-11 19:09:03 +0600

    Update c2021010022.lua

--- script/c2021010022.lua
+++ script/c2021010022.lua
@@ -54,9 +54,9 @@ function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsType(TYPE_MONSTER) then
+	if tc:IsRelateToEffect(e) and tc:GetType()==TYPE_MONSTER then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENCE)
-	elseif tc:IsRelateToEffect(e) and tc:IsType(TYPE_SPELL+TYPE_TRAP) then
+	elseif tc:IsRelateToEffect(e) and tc:GetType()==TYPE_SPELL+TYPE_TRAP then
 		Duel.SSet(tp,tc)
 	end
 end
