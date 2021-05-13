a815a019ea3a4c582ac064562820e4bbeb104e17
Almatik <almatiktk@gmail.com>
2021-05-13 14:42:09 +0600

    Add files via upload

--- script/c2021020001.lua
+++ script/c2021020001.lua
@@ -42,7 +42,7 @@ function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 		local p4=1-tp
 		local pt4=Duel.GetTurnCount()+4
 		local lp4=8000
-	elseif Duel.GetTurnCount()==2
+	elseif Duel.GetTurnCount()==2 then
 		--Player One
 		local p1=1-tp
 		local pt1=Duel.GetTurnCount()
