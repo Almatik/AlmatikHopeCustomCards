63567356b39200f2bbb79f44dcb678429c818443
Almatik <almatiktk@gmail.com>
2021-05-25 22:02:15 +0600

    Update c2021010001.lua

--- script/c2021010001.lua
+++ script/c2021010001.lua
@@ -59,6 +59,14 @@ end
 function s.tfilter(c,tp)
 	return c:IsOnField() and c:IsControler(tp)
 end
+function s.eqfilter(c,tc,tp)
+	if not (c:IsSetCard(0x107e) and not c:IsForbidden()) then return false end
+	local effs={c:GetCardEffect(75402014)}
+	for _,te in ipairs(effs) do
+		if te:GetValue()(tc,c,tp) then return true end
+	end
+	return false
+end
 function s.discon(e,tp,eg,ep,ev,re,r,rp)
 	if rp==tp or e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) then return false end
 	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
@@ -67,7 +75,7 @@ function s.discon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=e:GetHandler():GetOverlayGroup()
-	if chk==0 then return g:IsExists(s.mtfilter,1,nil) end
+	if chk==0 then return g:IsExists(s.eqfilter,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
@@ -76,7 +84,9 @@ end
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
-	local tc=c:GetOverlayGroup():FilterSelect(tp,s.mtfilter,1,1,nil)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local tc=c:GetOverlayGroup():FilterSelect(tp,s.eqfilter,1,1,nil)
 	if tc then
 		local eff=tc:GetCardEffect(75402014)
 		eff:GetOperation()(tc,eff:GetLabelObject(),tp,c)
