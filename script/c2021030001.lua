d048473d1376705739e98d62313b61cb731526ae
Almatik <almatiktk@gmail.com>
2021-05-28 22:17:06 +0600

    Create c2021030001.lua

---
+++ script/c2021030001.lua
@@ -0,0 +1,76 @@
+--Star Seraph Sword
+local s,id=GetID()
+function s.initial_effect(c)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--Grant effect to a "Constellar" Xyz monster using this card
+	local xe1=Effect.CreateEffect(c)
+	xe1:SetDescription(aux.Stringid(id,1))
+	xe1:SetCategory(CATEGORY_ATKCHANGE)
+	xe1:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_CONTINUOUS)
+	xe1:SetCondition(s.rmcon)
+	xe1:SetTarget(s.rmtg)
+	xe1:SetOperation(s.rmop)
+	c:RegisterEffect(xe1)
+end
+function s.cfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToDeckAsCost()
+end
+function s.thfilter(c,code)
+	return c:IsSetCard(0x86) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,e:GetHandler())
+	if g:GetFirst():IsLocation(LOCATION_HAND) then
+		Duel.ConfirmCards(1-tp,g)
+	end
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+
+
+
+	--Check if attached to "Constellar" Xyz monster
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	return r==REASON_XYZ and e:GetHandler():GetMaterial():IsExists(Card.IsPreviousLocation,3,nil,LOCATION_MZONE)
+end
+	--If this card battles a LIGHT/DARK monster, before damage calculation, banish it
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(s.atk)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsDisabled()
+end
+function s.atk(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)
+	return g:GetSum(Card.GetAttack)/2
+end
