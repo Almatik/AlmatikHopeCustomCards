232c5f8ae217cb691b4e164aac3e5df62fec00fc
Almatik <almatiktk@gmail.com>
2021-05-29 00:40:41 +0600

    Update c2021030002.lua

--- script/c2021030002.lua
+++ script/c2021030002.lua
@@ -13,6 +13,17 @@ function s.initial_effect(c)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
+	--Move itself to 1 of your unused MMZ
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.seqcon)
+	e3:SetTarget(s.seqtg)
+	e3:SetOperation(s.seqop)
+	c:RegisterEffect(e3)
 	
 end
 function s.spfilter(c,e,tp)
@@ -30,5 +41,58 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+
+
+	--Activation legality
+function s.seqcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsCode(49678559) or e:GetHandler():GetMaterial():IsExists(Card.IsCode,1,nil,49678559)
+end
+function s.seqtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
+	local seq=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0)
+	Duel.Hint(HINT_ZONE,tp,seq)
+	e:SetLabel(math.log(seq,2))
+end
+	--Move itself to 1 of your unused MMZ, then destroy all face-up cards in its new column
+function s.seqop(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local seq=e:GetLabel()
+	if not c:IsRelateToEffect(e) or c:IsControler(1-tp) or not Duel.CheckLocation(tp,LOCATION_MZONE,seq) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
+	Duel.MoveSequence(c,seq)
+	--Check for face-up cards in its current column, except itself
+	local ng=c:GetColumnGroup():Filter(Card.IsFaceup,c,nil)
+	local dg=c:GetColumnGroup():Filter(Card.IsDisabled,c,nil)
+	if #ng>0 then
+		Duel.BreakEffect()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		ng:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetValue(RESET_TURN_SET)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		ng:RegisterEffect(e2)
+		if ng:IsType(TYPE_TRAPMONSTER) then
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+			ng:RegisterEffect(e3)
+		end
+	end
+	if #dg>0 then
+		Duel.BreakEffect()
+		Duel.Destroy(dg,REASON_EFFECT)
 	end
 end
