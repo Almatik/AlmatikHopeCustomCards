b61af3bf6d1f0395fac3cde5fa1d904676ba1736
Almatik <almatiktk@gmail.com>
2021-06-06 20:44:52 +0600

    Create c2021030014.lua

---
+++ script/c2021030014.lua
@@ -0,0 +1,14 @@
+--Blue-Eyes Maximum Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+	Maximum.AddProcedure(c,nil,s.filter1,s.filter2)
+	c:AddMaximumAtkHandler()
+	c:AddCenterToSideEffectHandler(e1)
+end
+s.MaximumAttack=4500
+function s.filter1(c)
+	return c:IsCode(0xdd)
+end
+function s.filter2(c)
+	return c:IsCode(0xdd)
+end
