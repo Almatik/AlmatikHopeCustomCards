b35f0dd7578fa9eb96079d37c10f70a12de1ed77
Almatik <almatiktk@gmail.com>
2021-06-08 09:32:18 +0600

    Add files via upload

--- script/c2021040001.lua
+++ script/c2021040001.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--skill
-	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
+	aux.AddSkillProcedure(c,id,false,s.flipcon,s.flipop,1)
 	aux.GlobalCheck(s,function()
 		s[0]=nil
 		s[1]=nil
@@ -26,18 +26,22 @@ function s.filter(c)
 	return c:IsAbleToDeck()
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--twice per duel check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
 	--condition
-	return Duel.GetCurrentChain()==0 and s[2+tp]>=0
-		and Duel.IsExistingTarget(s.filter,tp,LOCATION_HAND,0,1,nil)
+	return aux.CanActivateSkill(tp) and s[2+tp]>=1500
 		and Duel.IsPlayerCanDraw(tp,1)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--place this card to the field
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	Duel.BreakEffect()
-	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil):GetFirst()
-	if Duel.SendtoDeck(tc,nil,0,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_DECK) then
-		if tc:IsLocation(LOCATION_DECK) then Duel.ShuffleDeck(tc:GetControler()) end
+	--Used skill flag register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	local c=e:GetHandler()
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
+	if #g>0 and Duel.SendtoDeck(g,nil,1,REASON_EFFECT)~=0 then
+		Duel.ShuffleDeck(tp)
 		Duel.BreakEffect()
 		Duel.Draw(tp,1,REASON_EFFECT)
 	end
