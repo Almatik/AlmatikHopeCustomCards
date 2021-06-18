523c85facdbad662b487d3337a2f065c10744a2d
Almatik <almatiktk@gmail.com>
2021-06-18 13:12:40 +0600

    Update c2021010043.lua

--- script/c2021010043.lua
+++ script/c2021010043.lua
@@ -1,4 +1,17 @@
 --Red-Eyes Dark Steel Dragon - Darkness Metal
-function c2021010043.initial_effect(c)
-	
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsCode,74677422),s.matfilter)
+	--spsummon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.fuslimit)
+	c:RegisterEffect(e1)
+end
+function s.matfilter(c,fc,st,tp)
+	return c:IsAttribute(ATTRIBUTE_DARK,fc,st,tp) or c:IsRace(RACE_MACHINE,fc,st,tp)
 end
