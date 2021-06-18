f96ca74da2e6817f8255aad8d04fa4fd148fc945
Almatik <almatiktk@gmail.com>
2021-06-18 13:38:46 +0600

    Update c2021010043.lua

--- script/c2021010043.lua
+++ script/c2021010043.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsCode,74677422),s.matfilter)
+	Fusion.AddProcMix(c,true,true,74677422,s.matfilter)
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
