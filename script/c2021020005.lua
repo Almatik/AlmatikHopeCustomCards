8a386a7718b27902c4437d047646a48c3b34a63e
Almatik <almatiktk@gmail.com>
2021-06-18 17:42:37 +0600

    Fix

--- script/c2021020005.lua
+++ script/c2021020005.lua
@@ -1,4 +1,30 @@
 --Yusei Go
-function c2021020005.initial_effect(c)
-	
+Duel.LoadScript("turboduel.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.TurboDuelActivate(c)
+	aux.TurboDuelIgnition(c,s.flipcon,s.flipop)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--condition
+	return aux.TBCanIgnition(tp) and c:IsCanRemoveCounter(tp,0x91,4,REASON_COST)
+		and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Smile World
+	c:RemoveCounter(tp,0x91,4,REASON_COST)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	if #g==0 then return end
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(200)
+		tc:RegisterEffect(e1)
+	end
 end
--- script/turboduel.lua
+++ script/turboduel.lua
@@ -1,65 +1,58 @@
-HINT_SKILL = 200
-HINT_SKILL_COVER = 201
-HINT_SKILL_FLIP  = 202
-HINT_SKILL_REMOVE = 203
--- Proc for basic skill
--- c: the card (card)
--- coverNum: the Number of the cover (int)
--- skillcon: condition to activate the skill (function)
--- skillop: operation related to the skill activation (function)
--- countlimit: number of times you can use this skill
--- skilltype: the type of the skill
--- setcode: the EVENT code
-function Auxiliary.TurboDuel(c,coverid,skillcon,skillop,countlimit)
-	--activate
-	local e1=Effect.CreateEffect(c) 
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_STARTUP)
-	e1:SetRange(0x5f)
-	e1:SetOperation(Auxiliary.DLSkillOp(coverid,skillcon,skillop,countlimit))
-	c:RegisterEffect(e1)
+
+function Auxiliary.TBCanIgnition(tp)
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnPlayer()==tp and (Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2)
 end
--- Skill Ignition
-function Auxiliary.TurboDuelIgnition(coverid,skillcon,skillop,countlimit,setcode)
+function Auxiliary.TBCanRemoveSpeedCounter(e,tp,ct)
+	return e:GetHandler():IsCanRemoveCounter(tp,0x91,ct,REASON_COST)
+end
+-- Proc for basic skill
+function Auxiliary.TurboDuelActivate(c)
+	c:EnableCounterPermit(0x91)
+	c:SetCounterLimit(0x91,12)
+	--add counter
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCountLimit(1)
+	e1:SetCode(EVENT_PHASE_START+PHASE_STANDBY)
+	e1:SetCondition(s.ctcon1)
+	e1:SetOperation(s.ctop1)
+	c:RegisterEffect(e1)
+	--add counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCountLimit(1)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCondition(s.ctcon2)
+	e2:SetOperation(s.ctop2)
+	c:RegisterEffect(e2)
+end
+function s.ctcon1(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsPlayerAffectedByEffect(tp,100100090)
+end
+function s.ctop1(e,tp,eg,ep,ev,re,r,rp)
+	local n=Duel.GetTurnCount()-1
+	e:GetHandler():AddCounter(0x91,1+n)
+end
+function s.ctcon2(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsPlayerAffectedByEffect(tp,100100090)
+		and eg:GetFirst():IsControler(tp)
+end
+function s.ctop2(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():AddCounter(0x91,1)
+end
+
+
+--Proc Ignition Skill
+function Auxiliary.TurboDuelIgnition(c,skillcon,skillop)
 	return function(e,tp,eg,ep,ev,re,r,rp)
 		local c=e:GetHandler()
-		--Ignition Effect
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_IGNITION)
-		e1:SetCode(EVENT_FREE_CHAIN)
-		e1:SetCondition(skillcon)
-		e1:SetOperation(skillop)
-		Duel.RegisterEffect(e1,e:GetHandlerPlayer())
-		Duel.DisableShuffleCheck(true)
-		--Turbo Effects
-		local te1=Effect.CreateEffect(c)
-		te1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER)
-		te1:SetCode(EVENT_PHASE)
-		te1:SetCondition(s.con1)
-		te1:SetOperation(s.op1)
-		Duel.RegisterEffect(te1,e:GetHandlerPlayer())
-		local te2=Effect.CreateEffect(c)
-		te2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER)
-		te2:SetCode(EVENT_SUMMON)
-		te2:SetCondition(s.con2)
-		te2:SetOperation(s.op2)
-		Duel.RegisterEffect(te2,e:GetHandlerPlayer())
-		local te3=Effect.CreateEffect(c)
-		te3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER)
-		te3:SetCode(EVENT_SUMMON)
-		te3:SetCondition(s.con2)
-		te3:SetOperation(s.op2)
-		Duel.RegisterEffect(te3,e:GetHandlerPlayer())
-		--remove it
-		Duel.SendtoDeck(c,tp,-2,REASON_RULE)
-		--generate the skill in the "skill zone"
-		Duel.Hint(HINT_SKILL_COVER,c:GetControler(),coverid)
-		Duel.Hint(HINT_SKILL,c:GetControler(),c:GetCode())
-		Duel.Hint(HINT_SKILL_FLIP,tp,c:GetCode()|(1<<32))
-		--send to limbo then draw 1 if the skill was in the hand
-		if e:GetHandler():IsPreviousLocation(LOCATION_HAND) then 
-			Duel.Draw(p,1,REASON_RULE)
+		if skillop~=nil then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_IGNITION)
+			e1:SetCode(EVENT_FREE_CHAIN)
+			e1:SetCondition(skillcon)
+			e1:SetOperation(skillop)
+			Duel.RegisterEffect(e1,e:GetHandlerPlayer())
 		end
 	end
 end
