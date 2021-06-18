089f1ed7e8c7a00d7cc3eb342ee0f36a45e704bd
Almatik <almatiktk@gmail.com>
2021-06-18 18:10:46 +0600

    Fix

--- script/c2021020005.lua
+++ script/c2021020005.lua
@@ -3,19 +3,50 @@ Duel.LoadScript("turboduel.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	aux.TurboDuelActivate(c)
 	aux.TurboDuelIgnition(c,s.flipcon,s.flipop)
+	c:EnableCounterPermit(0x91)
+	c:SetCounterLimit(0x91,12)
+	--add counter
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCountLimit(1)
+	e1:SetCode(EVENT_PHASE_START+PHASE_STANDBY)
+	e1:SetCondition(s.Condition1)
+	e1:SetOperation(s.Operation1)
+	c:RegisterEffect(e1)
+	--add counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCountLimit(1)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCondition(s.Condition2)
+	e2:SetOperation(s.Operation2)
+	c:RegisterEffect(e1)
+end
+function s.Condition1(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsPlayerAffectedByEffect(tp,100100090)
+end
+function s.Operation1(e,tp,eg,ep,ev,re,r,rp)
+	local n=Duel.GetTurnCount()-1
+	e:GetHandler():AddCounter(0x91,1+n)
+end
+function s.Condition2(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsPlayerAffectedByEffect(tp,100100090)
+		and eg:GetFirst():IsControler(tp)
+end
+function s.Operation2(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():AddCounter(0x91,1)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	--condition
-	return aux.TBCanIgnition(tp) and c:IsCanRemoveCounter(tp,0x91,0,REASON_COST)
+	return aux.TBCanIgnition(tp) and c:IsCanRemoveCounter(tp,0x91,1,REASON_COST)
 		and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	--Smile World
-	c:RemoveCounter(tp,0x91,0,REASON_COST)
+	c:RemoveCounter(tp,0x91,1,REASON_COST)
 	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
 	if #g==0 then return end
 	local tc=g:GetFirst()
--- script/turboduel.lua
+++ script/turboduel.lua
@@ -2,49 +2,6 @@
 function Auxiliary.TBCanIgnition(tp)
 	return Duel.GetCurrentChain()==0 and Duel.GetTurnPlayer()==tp and (Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2)
 end
--- Proc for basic skill
-function Auxiliary.TurboDuelActivate(c)
-	c:EnableCounterPermit(0x91)
-	c:SetCounterLimit(0x91,12)
-	--add counter
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCountLimit(1)
-	e1:SetCode(EVENT_PHASE_START+PHASE_STANDBY)
-	e1:SetCondition(TurboDuel.Condition1)
-	e1:SetOperation(TurboDuel.Operation1)
-	c:RegisterEffect(e1)
-	--add counter
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCountLimit(1)
-	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetCondition(TurboDuel.Condition2)
-	e2:SetOperation(TurboDuel.Operation2)
-	c:RegisterEffect(e1)
-end
-function TurboDuel.Condition1()
-	return function(e,tp,eg,ep,ev,re,r,rp)
-		return not Duel.IsPlayerAffectedByEffect(tp,100100090)
-	end
-end
-function TurboDuel.Operation1()
-	return function(e,tp,eg,ep,ev,re,r,rp)
-		local n=Duel.GetTurnCount()-1
-		e:GetHandler():AddCounter(0x91,1+n)
-	end
-end
-function TurboDuel.Condition2()
-	return function(e,tp,eg,ep,ev,re,r,rp)
-		return not Duel.IsPlayerAffectedByEffect(tp,100100090)
-			and eg:GetFirst():IsControler(tp)
-	end
-end
-function TurboDuel.Operation2()
-	return function(e,tp,eg,ep,ev,re,r,rp)
-		e:GetHandler():AddCounter(0x91,1)
-	end
-end
 
 
 --Proc Ignition Skill
