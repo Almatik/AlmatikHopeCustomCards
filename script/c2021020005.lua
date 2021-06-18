863b428bb7ffa4794a2eaaee1ef184402dbe14eb
Almatik <almatiktk@gmail.com>
2021-06-18 18:24:14 +0600

    Fix

--- script/c2021020005.lua
+++ script/c2021020005.lua
@@ -3,39 +3,7 @@ Duel.LoadScript("turboduel.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	aux.TurboDuelIgnition(c,s.flipcon,s.flipop)
-	c:EnableCounterPermit(0x91)
-	c:SetCounterLimit(0x91,12)
-	--add counter
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PHASE_START+PHASE_STANDBY)
-	e1:SetCountLimit(1)
-	e1:SetCondition(s.condition1)
-	e1:SetOperation(s.operation1)
-	c:RegisterEffect(e1)
-	--add counter
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetCountLimit(1)
-	e2:SetCondition(s.condition2)
-	e2:SetOperation(s.operation2)
-	c:RegisterEffect(e2)
-end
-function s.condition1(e,tp,eg,ep,ev,re,r,rp)
-	return not Duel.IsPlayerAffectedByEffect(tp,100100090)
-end
-function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-	local n=Duel.GetTurnCount()-1
-	e:GetHandler():AddCounter(0x91,1+n)
-end
-function s.condition2(e,tp,eg,ep,ev,re,r,rp)
-	return not Duel.IsPlayerAffectedByEffect(tp,100100090)
-		and eg:GetFirst():IsControler(tp)
-end
-function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():AddCounter(0x91,1)
+	aux.TurboDuelIgnition(c,id,s.flipcon,s.flipop)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
--- script/turboduel.lua
+++ script/turboduel.lua
@@ -5,13 +5,13 @@ end
 
 
 --Proc Ignition Skill
-function Auxiliary.TurboDuelStartUp(c,skillcon,skillop)
+function Auxiliary.TurboDuelStartUp(c,coverid,skillcon,skillop)
 	local e1=Effect.CreateEffect(c) 
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_STARTUP)
 	e1:SetRange(0x5f)
-	e1:SetOperation(Auxiliary.TurboDuelIgnition(skillcon,skillop))
+	e1:SetOperation(Auxiliary.TurboDuelIgnition(coverid,skillcon,skillop))
 	c:RegisterEffect(e1)	
 end
 function Auxiliary.TurboDuelIgnition(skillcon,skillop)
@@ -25,5 +25,8 @@ function Auxiliary.TurboDuelIgnition(skillcon,skillop)
 			e1:SetOperation(skillop)
 			Duel.RegisterEffect(e1,e:GetHandlerPlayer())
 		end
+		Duel.DisableShuffleCheck(true)
+		Duel.SendtoDeck(c,tp,-2,REASON_RULE)
+        Duel.Hint(HINT_SKILL_FLIP,tp,coverid|(1<<32))
 	end
 end
