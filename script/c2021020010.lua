af136f38331dbe0489c6145e36da6615bf7be39e
Almatik <almatiktk@gmail.com>
2021-06-21 00:02:14 +0600

    Fix

---
+++ script/c2021020010.lua
@@ -0,0 +1,47 @@
+--Storm Access
+Duel.LoadScript("duellinks.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.DuelLinksIgnition(c,2021040100,s.flipcon,s.flipop,1)
+	aux.GlobalCheck(s,function()
+		s[0]=nil
+		s[1]=nil
+		s[2]=0
+		s[3]=0
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_ADJUST)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end)
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if not s[tp] then s[tp]=Duel.GetLP(tp) end
+	if s[tp]>Duel.GetLP(tp) then
+		s[2+tp]=s[2+tp]+(s[tp]-Duel.GetLP(tp))
+		s[tp]=Duel.GetLP(tp)
+	end
+end
+function s.filter(c)
+	return c:IsAbleToDeck()
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return aux.CanActivateSkill(tp) and s[2+tp]>=0
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--place this card to the field
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	local c=e:GetHandler()
+	s.cyberse={TYPE_EXTRA,OPCODE_ISTYPE,TYPE_LINK,OPCODE_ISTYPE,OPCODE_AND,RACE_CYBERSE,OPCODE_ISRACE,OPCODE_AND}
+	if Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 then
+		local rn=Duel.GetRandomNumber(1,#s.cyberse)
+		local code=s.cyberse[rn]
+		local token=Duel.CreateToken(tp,code)
+		Duel.SpecialSummon(token,SUMMON_TYPE_LINK,tp,tp,false,false,POS_FACEUP)
+	end
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
+	s[2+tp]=0
+end
