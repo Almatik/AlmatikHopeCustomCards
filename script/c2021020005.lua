d246f6204f69824b490b603cae63b9d912a8c426
Almatik <almatiktk@gmail.com>
2021-06-21 15:54:34 +0600

    Add files via upload

--- script/c2021020005.lua
+++ script/c2021020005.lua
@@ -11,13 +11,9 @@ function s.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCountLimit(1)
 	e3:SetCode(EVENT_PHASE_START+PHASE_STANDBY)
-	e3:SetCondition(s.ctcon)
 	e3:SetOperation(s.ctop)
 	c:RegisterEffect(e3)
 
-end
-function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
-	return not Duel.IsPlayerAffectedByEffect(tp,100100090)
 end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	e:GetHandler():AddCounter(0x91,1)
