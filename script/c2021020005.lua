ec3372a2328d77d1feb095d066ff62a1e3f71bcf
Almatik <almatiktk@gmail.com>
2021-06-21 16:01:46 +0600

    Add files via upload

--- script/c2021020005.lua
+++ script/c2021020005.lua
@@ -5,10 +5,7 @@ function s.initial_effect(c)
 	c:EnableCounterPermit(0x91)
 	c:SetCounterLimit(0x91,12)
 	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
+	aux.TurboDuelStartUp(c,id)
 	--add counter
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
