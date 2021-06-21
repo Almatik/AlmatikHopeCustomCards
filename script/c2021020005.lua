eaa5e4a3b3bc35794ca1bead0bfa961a0321e33a
Almatik <almatiktk@gmail.com>
2021-06-21 15:59:25 +0600

    Add files via upload

--- script/c2021020005.lua
+++ script/c2021020005.lua
@@ -1,20 +1,19 @@
 --Yusei Go
+Duel.LoadScript("turboduel.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableCounterPermit(0x91)
 	c:SetCounterLimit(0x91,12)
 	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
+	aux.TurboDuelStartUp(c,id)
 	--add counter
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCountLimit(1)
-	e2:SetCode(EVENT_PHASE_START+PHASE_STANDBY)
-	e2:SetOperation(s.ctop)
-	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCountLimit(1)
+	e3:SetRange(LOCATION_ONFIELD)
+	e3:SetCode(EVENT_PHASE_START+PHASE_STANDBY)
+	e3:SetOperation(s.ctop)
+	c:RegisterEffect(e3)
 
 end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
