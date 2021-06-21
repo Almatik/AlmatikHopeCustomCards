4e3afe58c2838680f1925f788ed2418dc015479d
Almatik <almatiktk@gmail.com>
2021-06-21 16:05:34 +0600

    Add files via upload

--- script/turboduel.lua
+++ script/turboduel.lua
@@ -1,4 +1,7 @@
 
+function Auxiliary.TBCanIgnition(tp)
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnPlayer()==tp and (Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2)
+end
 
 
 --Proc Ignition Skill
@@ -13,7 +16,20 @@ function Auxiliary.TurboDuelStartUp(c,id)
 end
 function Auxiliary.TDStartUp(c,id)
 	return function(e,tp,eg,ep,ev,re,r,rp)
-		Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
-		Duel.Hint(HINT_CARD,tp,id)
+		SendtoDeck(c,tp,-2,REASON_RULE)
+		local tc=Duel.CreateToken(tp,id)
+		Duel.MoveToField(tc,tp,tp,LOCATION_FZONE,POS_FACEUP,true)
+		tc:EnableCounterPermit(0x91)
+		tc:SetCounterLimit(0x91,12)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e3:SetCountLimit(1)
+		e3:SetRange(LOCATION_ONFIELD)
+		e3:SetCode(EVENT_PHASE_START+PHASE_STANDBY)
+		e3:SetOperation(ctop)
+		tc:RegisterEffect(e3)
 	end
+end
+function ctop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():AddCounter(0x91,1)
 end
