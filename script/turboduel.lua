d8c31b0a5022d7e231f98b51b6f2321e43d94f44
Almatik <almatiktk@gmail.com>
2021-06-21 13:05:07 +0600

    Add files via upload

--- script/turboduel.lua
+++ script/turboduel.lua
@@ -18,18 +18,18 @@ function Auxiliary.TDStartUp(c,id)
 	return function(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 		Duel.Hint(HINT_CARD,tp,id)
-		if Duel.GetFlagEffect(tp,2021020004)==1 then n==1
-		elseif Duel.GetFlagEffect(tp,2021020004)==2 then n==2
-		elseif Duel.GetFlagEffect(tp,2021020004)==3 then n==3
-		elseif Duel.GetFlagEffect(tp,2021020004)==4 then n==4
-		elseif Duel.GetFlagEffect(tp,2021020004)==5 then n==5
-		elseif Duel.GetFlagEffect(tp,2021020004)==6 then n==6
-		elseif Duel.GetFlagEffect(tp,2021020004)==7 then n==7
-		elseif Duel.GetFlagEffect(tp,2021020004)==8 then n==8
-		elseif Duel.GetFlagEffect(tp,2021020004)==9 then n==9
-		elseif Duel.GetFlagEffect(tp,2021020004)==10 then n==10
-		elseif Duel.GetFlagEffect(tp,2021020004)==11 then n==11
-		elseif Duel.GetFlagEffect(tp,2021020004)==12 then n==12
+		if Duel.GetFlagEffect(tp,2021020004)=1 then n==1
+		elseif Duel.GetFlagEffect(tp,2021020004)=2 then n==2
+		elseif Duel.GetFlagEffect(tp,2021020004)=3 then n==3
+		elseif Duel.GetFlagEffect(tp,2021020004)=4 then n==4
+		elseif Duel.GetFlagEffect(tp,2021020004)=5 then n==5
+		elseif Duel.GetFlagEffect(tp,2021020004)=6 then n==6
+		elseif Duel.GetFlagEffect(tp,2021020004)=7 then n==7
+		elseif Duel.GetFlagEffect(tp,2021020004)=8 then n==8
+		elseif Duel.GetFlagEffect(tp,2021020004)=9 then n==9
+		elseif Duel.GetFlagEffect(tp,2021020004)=10 then n==10
+		elseif Duel.GetFlagEffect(tp,2021020004)=11 then n==11
+		elseif Duel.GetFlagEffect(tp,2021020004)=12 then n==12
 		end
 		--add counter
 		local e1=Effect.CreateEffect(c)
