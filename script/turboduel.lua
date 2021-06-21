cd0230eb6b831eaa8c5f2d8b27996d99ed7f1b13
Almatik <almatiktk@gmail.com>
2021-06-21 16:46:03 +0600

    Add files via upload

--- script/turboduel.lua
+++ script/turboduel.lua
@@ -5,7 +5,7 @@ end
 
 
 --Proc Ignition Skill
-function Auxiliary.TurboDuelStartUp(c,id)
+function Auxiliary.TurboDuelStart(c,id)
 	local e1=Effect.CreateEffect(c) 
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
