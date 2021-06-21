e649e55cd588922ab374de16a78f322ffd2bb97f
Almatik <almatiktk@gmail.com>
2021-06-21 16:08:22 +0600

    Add files via upload

--- script/c2021020005.lua
+++ script/c2021020005.lua
@@ -2,20 +2,8 @@
 Duel.LoadScript("turboduel.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	c:EnableCounterPermit(0x91)
-	c:SetCounterLimit(0x91,12)
 	--Activate
 	aux.TurboDuelStartUp(c,id)
 	--add counter
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCountLimit(1)
-	e3:SetRange(LOCATION_ONFIELD)
-	e3:SetCode(EVENT_PHASE_START+PHASE_STANDBY)
-	e3:SetOperation(s.ctop)
-	c:RegisterEffect(e3)
 
-end
-function s.ctop(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():AddCounter(0x91,1)
 end
