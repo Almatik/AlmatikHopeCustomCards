011cae87ffc4272c64101c8d9e36ad4f57671f94
Almatik <almatiktk@gmail.com>
2021-06-21 15:48:02 +0600

    Add files via upload

--- script/turboduel.lua
+++ script/turboduel.lua
@@ -1,7 +1,4 @@
 
-function Auxiliary.TBCanIgnition(tp)
-	return Duel.GetCurrentChain()==0 and Duel.GetTurnPlayer()==tp and (Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2)
-end
 
 
 --Proc Ignition Skill
@@ -16,23 +13,7 @@ function Auxiliary.TurboDuelStartUp(c,id)
 end
 function Auxiliary.TDStartUp(c,id)
 	return function(e,tp,eg,ep,ev,re,r,rp)
-		Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
-		Duel.Hint(HINT_CARD,tp,id)
-		c:EnableCounterPermit(0x91)
-		c:SetCounterLimit(0x91,12)
-		--add counter
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_PHASE_START+PHASE_STANDBY)
-		e1:SetCountLimit(1)
-		e1:SetCondition(ctcon)
-		e1:SetOperation(ctop)
-		Duel.RegisterEffect(e1,tp)
+		local code=Duel.CreateToken(tp,id)
+		Duel.MoveToField(code,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
 	end
-end
-function ctcon(e,tp,eg,ep,ev,re,r,rp)
-	return not Duel.IsPlayerAffectedByEffect(tp,100100090)
-end
-function ctop(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():AddCounter(0x91,1)
 end
