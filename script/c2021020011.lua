c3b8469bd4582882b5464a5ff3c1095a8b455cac
Almatik <almatiktk@gmail.com>
2021-06-23 18:23:25 +0600

    А

--- script/c2021020011.lua
+++ script/c2021020011.lua
@@ -8,7 +8,9 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--ask if you want to activate the skill or not
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	local sel={aux.Stringid(id,0),aux.Stringid(id,1)}
-	if Duel.SelectOption(1-tp,false,table.unpack(sel))~=0 then return end
+	local sel1={aux.Stringid(id,0)}
+	local sel2={aux.Stringid(id,1)}
+	Duel.SelectOption(tp,false,table.unpack(sel1))
+	Duel.SelectOption(tp,false,table.unpack(sel2))
 	aux.RegisterClientHint(e:GetHandler(),nil,1-tp,1,0,aux.Stringid(id,2),nil)
 end
