a1bd0a49cd7369932be3a831cb93a6e6653b44ed
Almatik <almatiktk@gmail.com>
2021-06-23 18:24:51 +0600

    Update c2021020011.lua

--- script/c2021020011.lua
+++ script/c2021020011.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--skill
-	aux.AddSkillProcedure(c,1,false,nil,s.flipop,1)
+	aux.AddSkillProcedure(c,1,false,true,s.flipop,1)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--ask if you want to activate the skill or not
