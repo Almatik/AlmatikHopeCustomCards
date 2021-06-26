1b47322a867ce2f06e148e235345d5ccc72746f4
Almatik <almatiktk@gmail.com>
2021-06-26 15:14:06 +0600

    Update c202001011.lua

--- script/c202001011.lua
+++ script/c202001011.lua
@@ -31,19 +31,17 @@ end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local lg=c:GetLinkedGroup()
-	if c:IsFaceup() and lg:IsFaceup() then
-		if c:IsRelatetoBattle() then
-			local tc=c
-			local atk=lg:GetAttack()
-		elseif lg:IsRelateToBattle() then
-			local tc=lg
-			local atk=c:GetAttack()
-		end
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
-		e1:SetValue(atk)
-		tc:RegisterEffect(e1)
+	if c:IsRelateToBattle() then
+		local tc=c
+		local atk=lg:GetAttack()
+	elseif lg:IsRelateToBattle() then
+		local tc=lg
+		local atk=c:GetAttack()
 	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
+	e1:SetValue(atk)
+	tc:RegisterEffect(e1)
 end
