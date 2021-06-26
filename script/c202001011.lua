ad411891ab958acc783568b29c254617ccd1c087
Almatik <almatiktk@gmail.com>
2021-06-26 15:11:00 +0600

    Update c202001011.lua

--- script/c202001011.lua
+++ script/c202001011.lua
@@ -4,7 +4,46 @@ function s.initial_effect(c)
 	--Link summon
 	Link.AddProcedure(c,nil,2,2,s.lcheck)
 	c:EnableReviveLimit()
+	--gain attack from special summoned card
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.atkcon)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
 end
 function s.lcheck(g,lc,sumtype,tp)
 	return g:IsExists(Card.IsSetCard,1,nil,0x2000,lc,sumtype,tp)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local lg=c:GetLinkedGroup()
+	local a=Duel.GetAttacker()
+	local b=a:GetBattleTarget()
+	if not b then return false end
+	if a:IsControler(1-tp) then a,b=b,a end
+	return a:GetControler()~=b:GetControler()
+			and (a==c or lg:IsContains(a))
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local lg=c:GetLinkedGroup()
+	if c:IsFaceup() and lg:IsFaceup() then
+		if c:IsRelatetoBattle() then
+			local tc=c
+			local atk=lg:GetAttack()
+		elseif lg:IsRelateToBattle() then
+			local tc=lg
+			local atk=c:GetAttack()
+		end
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
+		e1:SetValue(atk)
+		tc:RegisterEffect(e1)
+	end
 end
