63f471a195e5777d69799f8e16271ef2e40449e5
Almatik <almatiktk@gmail.com>
2021-06-28 19:31:44 +0600

    Update c2021030017.lua

--- script/c2021030017.lua
+++ script/c2021030017.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Ritual Summon
-	local e1=Ritual.CreateProc(c,RITPROC_GREATER,aux.FilterBoolFunction(Card.IsSetCard,0xb4),nil,aux.Stringid(id,0),nil,nil,s.ritfilter,nil,LOCATION_HAND+LOCATION_DECK,function(e,tp,g,sc) return not g:IsContains(e:GetHandler()), g:IsContains(e:GetHandler()) end)
+	local e1=Ritual.CreateProc(c,RITPROC_GREATER,s.ritfilter,nil,aux.Stringid(id,0),nil,nil,s.mfilter,nil,LOCATION_HAND+LOCATION_DECK,function(e,tp,g,sc) return not g:IsContains(e:GetHandler()), g:IsContains(e:GetHandler()) end)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_HAND)
@@ -13,6 +13,9 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.ritfilter(c)
+	return c:IsSetCard(0xb4) and not c:IsCode(id)
+end
+function s.mfilter(c)
 	return c:IsSetCard(0xb4) and not c:IsCode(id)
 end
 function s.ritcon(e,tp,eg,ep,ev,re,r,rp,chk)
