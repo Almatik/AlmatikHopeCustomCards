9b1dea973d225b4fb06ee4309aa7a352da0cf522
Almatik <almatiktk@gmail.com>
2021-07-04 20:19:08 +0600

    f

--- script/c2021030022.lua
+++ script/c2021030022.lua
@@ -1,6 +1,9 @@
 --Topoplogina Sassabee
 local s,id=GetID()
 function s.initial_effect(c)
+	--link summon
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_CYBERSE),2,2)
+	c:EnableReviveLimit()
 	--Banish itself it it leaves the field
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -8,5 +11,36 @@ function s.initial_effect(c)
 	e1:SetCondition(function(e)return e:GetHandler():IsFaceup()end)
 	e1:SetValue(LOCATION_REMOVED)
 	c:RegisterEffect(e1)
+	--Banish all cards on field
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.rmcon)
+	e2:SetTarget(s.rmtg)
+	e2:SetOperation(s.rmop)
+	c:RegisterEffect(e2)
 
+end
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	if eg:IsContains(e:GetHandler()) then return false end
+	local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_MZONE,LOCATION_MZONE,nil,TYPE_LINK)
+	for tc in aux.Next(g) do
+		if aux.zptgroupcon(eg,nil,tc) then return true end
+	end
+	return false
+end
+function s.rmfilter(c)
+	return c:IsAbleToRemove() and c:IsType(TYPE_MONSTER)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,0,0)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
 end
