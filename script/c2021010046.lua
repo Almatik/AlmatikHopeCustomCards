09acf72e02cd6071ec9163577efa643357e30113
Almatik <almatiktk@gmail.com>
2021-07-17 00:02:27 +0600

    Update c2021010046.lua

--- script/c2021010046.lua
+++ script/c2021010046.lua
@@ -48,22 +48,30 @@ function s.spfilter(c)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local num=1
-	if Duel.GetOverlayGroup():IsExists(s,spfilter,1,nil) then local num=2 end
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)>num-1
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,500,1,RACE_FIEND,ATTRIBUTE_DARK) then
-		repeat
-			local tc=Duel.CreateToken(tp,id+1)
-			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_SET_ATTACK)
-			e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-			e1:SetRange(LOCATION_MZONE)
-			e1:SetValue(s.atkval)
-			tc:RegisterEffect(e1)
-			num=num-1
-		until num==0
+		local tc=Duel.CreateToken(tp,id+1)
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetValue(s.atkval)
+		tc:RegisterEffect(e1)
+	end
+	if c:GetOverlayGroup():IsExists(s.spfilter,1,nil)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,500,1,RACE_FIEND,ATTRIBUTE_DARK) then
+		local tc=Duel.CreateToken(tp,id+1)
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetValue(s.atkval)
+		tc:RegisterEffect(e1)
 	end
 end
 function s.atkval(e,c)
