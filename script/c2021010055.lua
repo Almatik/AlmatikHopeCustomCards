7bd38d20c95231b1f263bd32bb35295b787baea7
Almatik <almatiktk@gmail.com>
2021-07-19 23:48:46 +0600

    f

--- script/c2021010055.lua
+++ script/c2021010055.lua
@@ -4,15 +4,109 @@ function s.initial_effect(c)
 	--pendulum summon
 	Pendulum.AddProcedure(c)
 	--splimit
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
-	e2:SetRange(LOCATION_PZONE)
-	e2:SetTargetRange(1,0)
-	e2:SetTarget(s.splimit)
-	c:RegisterEffect(e2)
+	local pe1=Effect.CreateEffect(c)
+	pe1:SetType(EFFECT_TYPE_FIELD)
+	pe1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	pe1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+	pe1:SetRange(LOCATION_PZONE)
+	pe1:SetTargetRange(1,0)
+	pe1:SetTarget(s.splimit)
+	c:RegisterEffect(pe1)
+	--Special Summon procedure
+	local me1=Effect.CreateEffect(c)
+	me1:SetDescription(aux.Stringid(id,0))
+	me1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	me1:SetType(EFFECT_TYPE_FIELD)
+	me1:SetRange(LOCATION_HAND)
+	me1:SetCode(EFFECT_SPSUMMON_PROC)
+	me1:SetCondition(s.hspcon)
+	me1:SetTarget(s.hsptg)
+	me1:SetOperation(s.hspop)
+	c:RegisterEffect(me1)
+	-- Search "Hieratic" card
+	local me2=Effect.CreateEffect(c)
+	me2:SetDescription(aux.Stringid(id,1))
+	me2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	me2:SetType(EFFECT_TYPE_IGNITION)
+	me2:SetRange(LOCATION_MZONE)
+	me2:SetCountLimit(1,id)
+	me2:SetCost(s.thcost)
+	me2:SetTarget(s.thtg)
+	me2:SetOperation(s.thop)
+	c:RegisterEffect(me2)
+	--If tributed, special summon 1 dragon normal monster from hand, deck, or GY
+	local me3=Effect.CreateEffect(c)
+	me3:SetDescription(aux.Stringid(id,2))
+	me3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	me3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	me3:SetCode(EVENT_RELEASE)
+	me3:SetTarget(s.sptg)
+	me3:SetOperation(s.spop)
+	c:RegisterEffect(me3)
 end
 function s.splimit(e,c)
 	return not c:IsSetCard(0x69)
+end
+function s.hspcon(e,c)
+	if c==nil then return true end
+	return Duel.CheckReleaseGroup(c:GetControler(),Card.IsSetCard,1,true,1,true,c,c:GetControler(),nil,false,e:GetHandler(),0x69)
+end
+function s.hsptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(tp,Card.IsSetCard,1,1,true,true,true,c,nil,nil,false,e:GetHandler(),0x69)
+	if g then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+	return true
+	end
+	return false
+end
+function s.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Release(g,REASON_COST)
+	g:DeleteGroup()
+end
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
+function s.thfilter(c)
+	return c:IsSetCard(0x69) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand() and not c:IsCode(id)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.spfilter(c,e,tp)
+	return c:IsType(TYPE_NORMAL) and c:IsRace(RACE_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0x13)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,0x13,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_SET_DEFENSE)
+		tc:RegisterEffect(e2)
+	end
+	Duel.SpecialSummonComplete()
 end
