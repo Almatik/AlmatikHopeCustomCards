cecf4fd911054cd58f781bcdfc4a094cf59cd981
Almatik <almatiktk@gmail.com>
2021-08-18 23:07:24 +0600

    f

---
+++ script/c202002205.lua
@@ -0,0 +1,57 @@
+--Sense of Battle Instincts
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x2010)
+end
+function s.desfilter(c,atk)
+	return c:IsFaceup() and c:GetAttack()<atk
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local g1=Duel.GetFieldGroup(tp,LOCATION_ONFIELD,0)
+	local g2=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) and #g2>#g1 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	local g1=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	local g2=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetValue(#g2*200)
+		tc:RegisterEffect(e1)
+		if #g1==1 then
+			local g=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_MZONE,nil,tc:GetBaseAttack())
+			if #g>0 then
+				Duel.Destroy(g,REASON_EFFECT)
+			end
+		end
+	end
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e2:SetTargetRange(1,0)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+end
+
+
