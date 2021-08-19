b54a41fa4ba9be6d1f684e5056960550a63ca7ab
Almatik <almatiktk@gmail.com>
2021-08-19 11:00:07 +0600

    Add files via upload

---
+++ script/c202002011.lua
@@ -0,0 +1,110 @@
+--Captain of the 11th Division - Kenpachi Zaraki
+function c202002011.initial_effect(c)
+	c:EnableUnsummonable()
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Activate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DISABLE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+	--Force battle
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_ANNOUNCE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(s.battletg)
+	e3:SetOperation(s.battleop)
+	c:RegisterEffect(e3)
+end
+function s.cfilter(c)
+	return c:GetSequence()<5
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	local rg=Duel.GetReleaseGroup(tp)
+	return (#g>0 or #rg>0) and g:FilterCount(Card.IsReleasable,nil)==#g 
+		and g:FilterCount(s.cfilter,nil)+Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.GetReleaseGroup(tp)
+	Duel.Release(g,REASON_COST)
+end
+
+
+
+
+
+
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c==Duel.GetAttacker() or c==Duel.GetAttackTarget()
+end
+function s.disfilter(c)
+	return (c:IsFaceup() or c:IsType(TYPE_TRAPMONSTER)) and not (c:IsType(TYPE_NORMAL) and c:GetOriginalType()&TYPE_NORMAL>0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if c:IsStatus(STATUS_LEAVE_CONFIRMED) then g:RemoveCard(c) end
+	if #g==0 then return end
+	Duel.BreakEffect()
+	local ng=g:Filter(s.disfilter,nil)
+	for nc in aux.Next(ng) do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		nc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		nc:RegisterEffect(e2)
+		if nc:IsType(TYPE_TRAPMONSTER) then
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			nc:RegisterEffect(e3)
+		end
+	end
+end
+
+
+
+
+
+
+
+
+function s.battletg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function s.battleop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.CalculateDamage(c,tc)
+	end
+end
