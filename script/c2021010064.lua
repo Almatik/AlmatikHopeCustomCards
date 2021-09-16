88632b8763f4bd31925582ceb9bbef64ef412eda
Almatik <almatiktk@gmail.com>
2021-09-16 22:13:54 +0600

    f

--- script/c2021010064.lua
+++ script/c2021010064.lua
@@ -1,4 +1,86 @@
---Opposite Zoodiac
-function c2021010064.initial_effect(c)
-	
+--Opposite Zoodiaclocal s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_BEASTWARRIOR),5,2,s.ovfilter,aux.Stringid(id,0),2,s.xyzop)
+	c:EnableReviveLimit()
+	--xyzlimit
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
+	e0:SetValue(1)
+	c:RegisterEffect(e0)
+	--atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.atkval)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	e2:SetValue(s.defval)
+	c:RegisterEffect(e2)
+	--search
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetCondition(s.thcon)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
+
+
 end
+s.listed_series={0xf1}
+function s.ovfilter(c,tp,lc)
+	return c:IsFaceup() and c:IsSetCard(0xf1,lc,SUMMON_TYPE_XYZ,tp) and not c:IsSummonCode(lc,SUMMON_TYPE_XYZ,tp,id)
+end
+function s.xyzop(e,tp,chk)
+	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 end
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+	return true
+end
+function s.atkfilter(c)
+	return c:IsSetCard(0xf1) and c:GetAttack()>=0
+end
+function s.atkval(e,c)
+	local g=e:GetHandler():GetOverlayGroup():Filter(s.atkfilter,nil)
+	return g:GetSum(Card.GetAttack)
+end
+function s.deffilter(c)
+	return c:IsSetCard(0xf1) and c:GetDefense()>=0
+end
+function s.defval(e,c)
+	local g=e:GetHandler():GetOverlayGroup():Filter(s.deffilter,nil)
+	return g:GetSum(Card.GetDefense)
+end
+
+
+
+
+
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp~=tp
+end
+function s.thfilter(c)
+	return c:IsSetCard(0xf1) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
