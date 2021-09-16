1dcc0bf4cff8870372295135498d847f46466d00
Almatik <almatiktk@gmail.com>
2021-09-16 15:42:11 +0600

    Add files via upload

--- script/c2021010029.lua
+++ script/c2021010029.lua
@@ -12,17 +12,37 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--Grant effect to "Utopia "Xyz monster using this card as material
+	--Cannot be destroyed by card effects
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_BE_MATERIAL)
-	e2:SetProperty(EFFECT_FLAG_EVENT_PLAYER)
-	e2:SetCountLimit(1,id)
-	e2:SetCondition(s.efcon)
-	e2:SetOperation(s.efop)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetType(EFFECT_TYPE_XMATERIAL)
+	e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetCondition(s.xcon)
+	e2:SetValue(1)
 	c:RegisterEffect(e2)
+	--Negate monster effect
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,3))
+	e3:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.xcon1)
+	e3:SetCost(s.xcost1)
+	e3:SetTarget(s.xtg1)
+	e3:SetOperation(s.xop1)
+	c:RegisterEffect(e3)
 end
 
+function s.mfilter(c)
+	return c:IsFaceup() not c:IsType(TYPE_TOKEN)
+end
+function s.xyzfilter(c,mg)
+	return c:IsSetCard(0x107f) and c:IsXyzSummonable(nil,mg)
+end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE) end
@@ -33,59 +53,54 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if c:IsRelateToEffect(e) then
 		if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
 			Duel.NegateAttack()
+			local g=Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_MZONE,0,nil)
+			if Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_EXTRA,0,1,nil,g) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+				local xyzg=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,g)
+				if #xyzg>0 then
+					Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+					local xyz=xyzg:Select(tp,1,1,nil):GetFirst()
+					Duel.XyzSummon(tp,xyz,nil,g,1,99)
+				end
+			end
 		end
 	end
 end
 
 
-	--If an "Utopia" Xyz used this card as material
-function s.efcon(e,tp,eg,ep,ev,re,r,rp)
+
+
+
+
+
+
+function s.xcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return r==REASON_XYZ and c:GetReasonCard():IsSetCard(0x107f)
+	return c:IsSetCard(0x107f) and c:IsType(TYPE_XYZ)
 end
-	--Grant effect to "Utopia "Xyz monster using this card as material
-function s.efop(e,tp,eg,ep,ev,re,r,rp)
+function s.xtfilter(c)
+	return c:IsLocation(LOCATION_MZONE)
+end
+function s.xcost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.xcon1(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local rc=c:GetReasonCard()
-	--Cannot be destroyed by card effects
-	local e1=Effect.CreateEffect(rc)
-	e1:SetDescription(aux.Stringid(id,1))
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e1:SetValue(1)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	rc:RegisterEffect(e1)
-	--Inflict damage equal to the destroyed monster's ATK
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,2))
-	e2:SetCategory(CATEGORY_DAMAGE)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e2:SetCode(EVENT_BATTLE_DESTROYING)
-	e2:SetCondition(aux.bdgcon)
-	e2:SetTarget(s.damtg)
-	e2:SetOperation(s.damop)
-	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-	rc:RegisterEffect(e2)
+	return c:IsSetCard(0x107f) and c:IsType(TYPE_XYZ)
+	if c:IsStatus(STATUS_BATTLE_DESTROYED) then return false end
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	return tg and tg:IsExists(s.xtfilter,1,nil) and Duel.IsChainNegatable(ev)
 end
-function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.xtg1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local bc=e:GetHandler():GetBattleTarget()
-	Duel.SetTargetCard(bc)
-	local dam=bc:GetAttack()
-	if dam<0 then dam=0 end
-	Duel.SetTargetPlayer(1-tp)
-	Duel.SetTargetParam(dam)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
 end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-		local dam=tc:GetAttack()
-		if dam<0 then dam=0 end
-		Duel.Damage(p,dam,REASON_EFFECT)
+function s.xop1(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
 	end
 end
