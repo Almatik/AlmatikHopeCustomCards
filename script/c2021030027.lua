a5da1717da6a03388da74d9758f41078b7ddd0e2
Almatik <almatiktk@gmail.com>
2021-10-19 14:46:33 +0600

    Add files via upload

--- script/c2021030027.lua
+++ script/c2021030027.lua
@@ -125,6 +125,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	local sel={}
 	if tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,tp)
