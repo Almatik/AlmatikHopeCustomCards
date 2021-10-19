e9bc051d18649766afc92a4caeb1956ce5cc6b96
Almatik <almatiktk@gmail.com>
2021-10-19 13:55:10 +0600

    Add files via upload

---
+++ script/c2021030027.lua
@@ -0,0 +1,249 @@
+--Dragulas the Vampiric Dragon King
+local s,id=GetID()
+function s.initial_effect(c)
+	--Link Summon
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,nil,3,4,s.lcheck)
+	local ls0=Effect.CreateEffect(c)
+	ls0:SetType(EFFECT_TYPE_FIELD)
+	ls0:SetRange(LOCATION_EXTRA)
+	ls0:SetCode(EFFECT_EXTRA_MATERIAL)
+	ls0:SetProperty(EFFECT_FLAG_PLAYER_TARGET|EFFECT_FLAG_CANNOT_DISABLE|EFFECT_FLAG_SET_AVAILABLE)
+	ls0:SetTargetRange(1,1)
+	ls0:SetOperation(s.extracon)
+	ls0:SetValue(s.extraval)
+	c:RegisterEffect(ls0)
+	--If "0" then Special Summon
+	local e0=Effect.CreateEffect(c)
+	e0:SetDescription(aux.Stringid(id,0))
+	e0:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e0:SetType(EFFECT_TYPE_IGNITION)
+	e0:SetRange(LOCATION_MZONE)
+	e0:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e0:SetCountLimit(1,id)
+	e0:SetTarget(s.sptg)
+	e0:SetOperation(s.spop)
+	c:RegisterEffect(e0)
+	--If "1" then OMNI Negate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,3))
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCountLimit(1)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.discon)
+	e1:SetCost(s.discost)
+	e1:SetTarget(s.distg)
+	e1:SetOperation(s.disop)
+	c:RegisterEffect(e1)
+	--If "2" then cannot be used as Materials
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,4))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL+EFFECT_CANNOT_BE_SYNCHRO_MATERIAL+EFFECT_CANNOT_BE_XYZ_MATERIAL+EFFECT_CANNOT_BE_LINK_MATERIAL)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetCondition(s.effcon)
+	e2:SetTarget(s.efftg)
+	e2:SetValue(s.efflimit)
+	c:RegisterEffect(e2)
+	--If "3" then cannot be Effect Target
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,5))
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetCondition(s.etcon)
+	e3:SetTarget(s.ettg)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	--If "4" then cannot be Effect Target
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,6))
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e4:SetCode(EFFECT_INDESTRUCTABLE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e4:SetCondition(s.undcon)
+	e4:SetTarget(s.undtg)
+	e4:SetValue(1)
+	c:RegisterEffect(e4)
+	
+end
+function s.lcheck(g,lc,sumtype,tp)
+	return c:IsType(TYPE_XYZ,lc,sumtype,tp) and c:IsSetCard(0x8e,lc,sumtype,tp)
+end
+s.curgroup=nil
+function s.extracon(c,e,tp,sg,mg,lc,og,chk)
+	return not s.curgroup or #(sg&s.curgroup)<2
+end
+function s.extraval(chk,summon_type,e,...)
+	if chk==0 then
+		local tp,sc=...
+		if summon_type~=SUMMON_TYPE_LINK or sc~=e:GetHandler() then
+			return Group.CreateGroup()
+		else
+			s.curgroup=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+			s.curgroup:KeepAlive()
+			return s.curgroup
+		end
+	elseif chk==2 then
+		if s.curgroup then
+			s.curgroup:DeleteGroup()
+		end
+		s.curgroup=nil
+	end
+end
+
+
+
+
+
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetLinkedGroup():GetCount()>=0
+end
+function s.spfilter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,tp)
+		or c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,1-tp)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE,tp)>0
+						or Duel.GetLocationCount(1-tp,LOCATION_MZONE,1-tp)>0
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local sel={}
+	if tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,tp)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE,tp)>0 then
+		table.insert(sel,aux.Stringid(id,1))
+	end
+	if tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE,1-tp)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE,1-tp)>0 then
+		table.insert(sel,aux.Stringid(id,2))
+	end
+	local res=Duel.SelectOption(tp,false,table.unpack(sel))
+	if res==0 then
+		if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_DISABLE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_DISABLE_EFFECT)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e2)
+		end
+		Duel.SpecialSummonComplete()
+	else
+		if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,1-tp,false,false,POS_FACEUP_DEFENSE) then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_DISABLE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_DISABLE_EFFECT)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e2)
+		end
+		Duel.SpecialSummonComplete()
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return rp~=tp and not c:IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev)
+		and c:GetLinkedGroup():GetCount()>=1
+end
+function s.disfilter(c,g)
+	return g:IsContains(c) and c:IsReleasable()
+end
+function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local lg=e:GetHandler():GetLinkedGroup()
+	local rg=Duel.GetMatchingGroup(s.disfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,lg)
+	if chk==0 then return #rg>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g=rg:Select(tp,1,1,nil)
+	Duel.Release(g,REASON_COST)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
+
+
+
+
+
+
+
+function s.effcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetLinkedGroup():GetCount()>=2
+end
+function s.efftg(e,c)
+	return e:GetHandler()==c or e:GetHandler():GetLinkedGroup():IsContains(c)
+end
+function s.efflimit(e,c)
+	if not c then return false end
+	return not c:IsSetCard(0x8e)
+end
+
+
+
+
+
+
+
+function s.etcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetLinkedGroup():GetCount()>=3
+end
+function s.ettg(e,c)
+	return e:GetHandler()==c or (c:IsSetCard(0x8e) and e:GetHandler():GetLinkedGroup():IsContains(c))
+end
+
+
+
+
+
+function s.undcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetLinkedGroup():GetCount()==4
+end
+function s.undtg(e,c)
+	return e:GetHandler()==c or (c:IsSetCard(0x8e) and c:IsType(TYPE_XYZ) and e:GetHandler():GetLinkedGroup():IsContains(c))
+end
