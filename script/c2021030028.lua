32854a51f05019e2daa51e90a978251fb18085ce
Almatik <almatiktk@gmail.com>
2021-10-19 19:07:59 +0600

    Update c2021030028.lua

--- script/c2021030028.lua
+++ script/c2021030028.lua
@@ -14,6 +14,17 @@ function s.initial_effect(c)
 	e1:SetOperation(s.extracon)
 	e1:SetValue(s.extraval)
 	c:RegisterEffect(e1)
+	--Activate Malefic Territori
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.accon)
+	e2:SetOperation(s.acop)
+	c:RegisterEffect(e2)
+
 
 
 	
@@ -41,4 +52,20 @@ function s.extraval(chk,summon_type,e,...)
 		end
 		s.curgroup=nil
 	end
+end
+
+
+
+
+function s.accon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK) and Duel.IsExistingMatchingCard(s.fieldfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,tp)
+end
+function s.fieldfilter(c,tp)
+	return c:IsCode(75223115) and c:GetActivateEffect() and c:GetActivateEffect():IsActivatable(tp,true,true)
+end
+function s.acop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local tc=Duel.SelectMatchingCard(tp,s.fieldfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,tp):GetFirst()
+	aux.PlayFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
 end
