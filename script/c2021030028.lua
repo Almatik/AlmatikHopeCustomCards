b6116ea10683b8ebea8dc8140495fb8203237262
Almatik <almatiktk@gmail.com>
2021-11-14 14:07:40 +0600

    Update c2021030028.lua

--- script/c2021030028.lua
+++ script/c2021030028.lua
@@ -174,8 +174,6 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 		if Duel.GetControl(tg,tp)>0 then
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-			e1:SetRange(LOCATION_MZONE)
 			e1:SetCode(EFFECT_CHANGE_CODE)
 			e1:SetValue(2021030029)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
