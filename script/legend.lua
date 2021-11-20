8b5e6810570166d7200a07ce1a850d41acf45414
Almatik <almatiktk@gmail.com>
2021-11-20 17:01:36 +0600

    Update legend.lua

--- script/legend.lua
+++ script/legend.lua
@@ -2,15 +2,8 @@ TYPE_LEGEND      = 0x100000000
 REASON_LEGEND        = 0x100000000
 SUMMON_TYPE_LEGEND     = 0x100000000
 
-if not aux.LegendProcedure then
-	aux.LegendProcedure = {}
-	Legend = aux.LegendProcedure
-end
-if not Legend then
-	Legend = aux.LegendProcedure
-end
 --Legend Summon
-function Legend.AddProcedure(c,mat,desc)
+function aux.LegendProcedure(c,mat,desc)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	if desc then
@@ -21,13 +14,13 @@ function Legend.AddProcedure(c,mat,desc)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
 	e1:SetRange(LOCATION_EXTRA)
-	e1:SetCondition(Legend.Condition(mat))
-	e1:SetTarget(Legend.Target(mat))
-	e1:SetOperation(Legend.Operation(mat))
+	e1:SetCondition(aux.LegendCondition(mat))
+	e1:SetTarget(aux.LegendTarget(mat))
+	e1:SetOperation(aux.LegendOperation(mat))
 	e1:SetValue(SUMMON_TYPE_LEGEND)
 	c:RegisterEffect(e1)
 end
-function legend.Condition(mat)
+function aux.LegendCondition(mat)
 	return function(e,c)
 		if c==nil then return true end
 		local tp=c:GetControler()
@@ -35,7 +28,7 @@ function legend.Condition(mat)
 		return #rg>0 and aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),0)
 	end
 end
-function Legend.Target(mat)
+function aux.LegendTarget(mat)
 	return function(e,tp,eg,ep,ev,re,r,rp)
 		local c=e:GetHandler()
 		local g=nil
@@ -49,7 +42,7 @@ function Legend.Target(mat)
 		return false
 	end
 end
-function Legend.Operation(mat)
+function aux.LegendOperation(mat)
 	return function(e,tp,eg,ep,ev,re,r,rp,c)
 		local g=e:GetLabelObject()
 		local c=e:GetHandler()
