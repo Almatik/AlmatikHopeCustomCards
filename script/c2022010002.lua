b66c3ce75efb1b165d08d1e210c560c69c79c208
Almatik <almatiktk@gmail.com>
2021-11-20 18:03:26 +0600

    а

--- script/c2022010002.lua
+++ script/c2022010002.lua
@@ -5,13 +5,13 @@ function s.initial_effect(c)
 	aux.LegendProcedure(c,s.material,s.operation,EVENT_SPSUMMON_SUCCESS)
 end
 function s.material(c)
-	return c:IsCode(2022010001) and Duel.GetFlagEffect(tp,id)>2
+	return c:IsCode(2022010001) and Duel.GetFlagEffect(tp,id)>0
 end
 function s.filter(c)
 	return c:IsCode(2022010003)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetFlagEffect(tp,id)>2 then return end
+	if Duel.GetFlagEffect(tp,id)>0 then return end
 	--condition
 	if eg:IsExists(s.filter,1,nil) then
 		--Set Flag Effect
