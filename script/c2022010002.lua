cd0f4dfa5156e33e3cc086830a716ae0df6f7340
Almatik <almatiktk@gmail.com>
2021-11-20 17:48:21 +0600

    Update c2022010002.lua

--- script/c2022010002.lua
+++ script/c2022010002.lua
@@ -2,8 +2,31 @@
 Duel.LoadScript("legend.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.LegendProcedure(c,s.material)
+	aux.LegendProcedure(c,s.material,s.markcon,s.markop,EVENT_SPSUMMON)
 end
 function s.material(c)
-	return c:IsCode(2022010001)
+	return c:IsCode(2022010001) and Duel.GetFlagEffect(tp,id)>2
+end
+function s.markcon(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(tp,id)>2 then return end
+	--condition
+	return Duel.GetCurrentChain()==0 and tp==Duel.GetTurnPlayer()
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp,tid)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--ask if you want to activate the skill or not
+	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+		Duel.Hint(HINT_CARD,tp,id)
+		--Draw
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_DRAW_COUNT)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetTargetRange(1,0)
+		e1:SetValue(2)
+		e1:SetReset(RESET_PHASE+PHASE_DRAW)
+		Duel.RegisterEffect(e1,tp)
+		Duel.RegisterFlagEffect(tp,id,0,0,0)
+	end
 end
