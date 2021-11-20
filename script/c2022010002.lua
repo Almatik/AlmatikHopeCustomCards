8a1241079482135d394739ac0f78bbe20ac1229b
Almatik <almatiktk@gmail.com>
2021-11-20 18:10:38 +0600

    f

--- script/c2022010002.lua
+++ script/c2022010002.lua
@@ -2,18 +2,21 @@
 Duel.LoadScript("legend.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.LegendProcedure(c,s.material,s.condition,s.operation,EVENT_SPSUMMON_SUCCESS)
+	aux.LegendProcedure(c,s.matcon,s.matm,s.markcon,s.markop,EVENT_SPSUMMON_SUCCESS)
 end
-function s.material(c)
-	return c:IsCode(2022010001) and Duel.GetFlagEffect(tp,id)>0
+function s.matcon(e,tp)
+	return Duel.GetFlagEffect(tp,id)>0
+end
+function s.matm(c)
+	return c:IsCode(2022010001)
 end
 function s.filter(c)
 	return c:IsCode(2022010003)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
+function s.markcon(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFlagEffect(tp,id)>0 then return end
 	return eg:IsExists(s.filter,1,nil)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.markop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RegisterFlagEffect(tp,id)
 end
--- script/legend.lua
+++ script/legend.lua
@@ -3,15 +3,15 @@ REASON_LEGEND        = 0x100000000
 SUMMON_TYPE_LEGEND     = 0x100000000
 
 --Legend Summon
-function aux.LegendProcedure(c,mat,markcon,markop,setcode)
+function aux.LegendProcedure(c,matcon,matm,markcon,markop,setcode)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
 	e1:SetRange(LOCATION_HAND+LOCATION_DECK)
-	e1:SetCondition(aux.LegendCondition(mat))
-	e1:SetTarget(aux.LegendTarget(mat))
-	e1:SetOperation(aux.LegendOperation(mat))
+	e1:SetCondition(aux.LegendCondition(matcon,matm))
+	e1:SetTarget(aux.LegendTarget(matm))
+	e1:SetOperation(aux.LegendOperation(matm))
 	e1:SetValue(SUMMON_TYPE_LEGEND)
 	c:RegisterEffect(e1)
 	if not markcon and not markop then return end
@@ -24,20 +24,20 @@ function aux.LegendProcedure(c,mat,markcon,markop,setcode)
 	e2:SetOperation(markop)
 	c:RegisterEffect(e2)
 end
-function aux.LegendCondition(mat)
+function aux.LegendCondition(matcon,matm)
 	return function(e,c)
 		if c==nil then return true end
 		local tp=c:GetControler()
-		local rg=Duel.GetMatchingGroup(mat,tp,LOCATION_MZONE,0,nil)
+		local rg=Duel.GetMatchingGroup(matm,tp,LOCATION_MZONE,0,nil)
 		local code=c:GetCode()
-		return #rg>0 and aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),0)
+		return #rg>0 and aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),0) and matcon
 	end
 end
-function aux.LegendTarget(mat)
+function aux.LegendTarget(matm)
 	return function(e,tp,eg,ep,ev,re,r,rp)
 		local c=e:GetHandler()
 		local g=nil
-		local rg=Duel.GetMatchingGroup(mat,tp,LOCATION_MZONE,0,nil)
+		local rg=Duel.GetMatchingGroup(matm,tp,LOCATION_MZONE,0,nil)
 		local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_TOGRAVE,nil,nil,true)
 		if #g>0 then
 			g:KeepAlive()
@@ -47,7 +47,7 @@ function aux.LegendTarget(mat)
 		return false
 	end
 end
-function aux.LegendOperation(mat)
+function aux.LegendOperation(matm)
 	return function(e,tp,eg,ep,ev,re,r,rp,c)
 			local g=e:GetLabelObject()
 			if not g then return end
