3b6fe9b4c9fa8caffa7bfca82ef2a6165d6e2b8d
Almatik <almatiktk@gmail.com>
2021-11-20 17:19:27 +0600

    Update legend.lua

--- script/legend.lua
+++ script/legend.lua
@@ -3,14 +3,9 @@ REASON_LEGEND        = 0x100000000
 SUMMON_TYPE_LEGEND     = 0x100000000
 
 --Legend Summon
-function aux.LegendProcedure(c,mat,desc)
+function aux.LegendProcedure(c,mat,markcon,markop,setcode)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	if desc then
-		e1:SetDescription(desc)
-	else
-		e1:SetDescription(1174)
-	end
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
 	e1:SetRange(LOCATION_EXTRA)
@@ -19,12 +14,21 @@ function aux.LegendProcedure(c,mat,desc)
 	e1:SetOperation(aux.LegendOperation(mat))
 	e1:SetValue(SUMMON_TYPE_LEGEND)
 	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c) 
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(setcode)
+	e1:SetRange(LOCATION_EXTRA)
+	e1:SetCondition(markcon)
+	e1:SetOperation(markop)
+	c:RegisterEffect(e1)
 end
 function aux.LegendCondition(mat)
 	return function(e,c)
 		if c==nil then return true end
 		local tp=c:GetControler()
 		local rg=Duel.GetMatchingGroup(mat,tp,LOCATION_MZONE,0,nil)
+		local code=c:GetCode()
 		return #rg>0 and aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),0)
 	end
 end
@@ -40,15 +44,5 @@ function aux.LegendTarget(mat)
 			return true
 		end
 		return false
-	end
-end
-function aux.LegendOperation(mat)
-	return function(e,tp,eg,ep,ev,re,r,rp,c)
-		local g=e:GetLabelObject()
-		local c=e:GetHandler()
-		if not g then return end
-		c:SetMaterial(g)
-		Duel.SendtoGrave(g,REASON_LEGEND)
-		g:DeleteGroup()
 	end
 end
