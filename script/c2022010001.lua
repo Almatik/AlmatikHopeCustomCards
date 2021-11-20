46fa43f424e7ab871e8690cd4a58e88a84586029
Almatik <almatiktk@gmail.com>
2021-11-20 17:02:15 +0600

    Update c2022010001.lua

--- script/c2022010001.lua
+++ script/c2022010001.lua
@@ -2,7 +2,7 @@
 Duel.LoadScript("legend.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	Legend.AddProcedure(c,s.material)
+	aux.LegendProcedure(c,s.material)
 end
 function s.material(c)
 	return c:IsCode(2022010002)
