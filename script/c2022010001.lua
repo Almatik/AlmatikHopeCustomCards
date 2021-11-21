631092fa6e1f5c18bce4ff88f92a906aa3198bed
Almatik <almatiktk@gmail.com>
2021-11-21 23:35:00 +0600

    Update c2022010001.lua

--- script/c2022010001.lua
+++ script/c2022010001.lua
@@ -2,16 +2,16 @@
 Duel.LoadScript("legend.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.LegendProcedure(c,id,0,s.mat,s.mark,EVENT_SPSUMMON_SUCCESS)
+	aux.LegendProcedure(c,id,1,s.mat,s.mark,EVENT_SPSUMMON_SUCCESS)
 end
 function s.mat(c)
-	return c:IsCode(2022010002)
+	return c:IsCode(id+1)
 end
 function s.filter(c,tp)
-	return c:IsCode(2022010003) and c:IsControler(tp)
+	return c:IsCode(id+2) and c:IsControler(tp)
 end
 function s.mark(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetFlagEffect(tp,id)>=3 and eg:IsExists(s.filter,1,nil,tp) then
+	if Duel.GetFlagEffect(tp,id)<3 and eg:IsExists(s.filter,1,nil,tp) then
 		Duel.RegisterFlagEffect(tp,id,0,0,0)
 	end
 end
