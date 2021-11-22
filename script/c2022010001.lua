52e67e7a98346186a49e12c5651c50db0169ddea
Almatik <almatiktk@gmail.com>
2021-11-22 17:15:57 +0600

    Update c2022010001.lua

--- script/c2022010001.lua
+++ script/c2022010001.lua
@@ -3,6 +3,18 @@ Duel.LoadScript("legend.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.LegendProcedure(c,id,3,s.mat,s.mark,EVENT_SPSUMMON_SUCCESS)
+	--Special Summon 1 monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+
 end
 function s.mat(c)
 	return c:IsCode(id+1)
@@ -14,4 +26,60 @@ function s.mark(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFlagEffect(tp,id)<3 and eg:IsExists(s.filter,1,nil,tp) then
 		Duel.RegisterFlagEffect(tp,id,0,0,0)
 	end
+end
+
+
+
+
+
+
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+2,0,TYPES_TOKEN,1500,0,3,RACE_WARRIOR,ATTRIBUTE_DARK,POS_FACEUP) end local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,ft,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,ft,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 or not Duel.IsPlayerCanSpecialSummonMonster(tp,id+2,0,TYPES_TOKEN,1500,0,3,RACE_WARRIOR,ATTRIBUTE_DARK,POS_FACEUP) then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	local fid=e:GetHandler():GetFieldID()
+	local g=Group.CreateGroup()
+	for i=1,ft do
+		local token=Duel.CreateToken(tp,id+2)
+		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
+		token:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE,0,1,fid)
+		g:AddCard(token)
+	end
+	Duel.SpecialSummonComplete()
+	g:KeepAlive()
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetReset(RESET_PHASE+PHASE_BATTLE)
+	e1:SetCountLimit(1)
+	e1:SetLabel(fid)
+	e1:SetLabelObject(g)
+	e1:SetCondition(s.descon)
+	e1:SetOperation(s.desop)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.desfilter(c,fid)
+	return c:GetFlagEffectLabel(id)==fid
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	if not g:IsExists(s.desfilter,1,nil,e:GetLabel()) then
+		g:DeleteGroup()
+		e:Reset()
+		return false
+	else return true end
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	local tg=g:Filter(s.desfilter,nil,e:GetLabel())
+	g:DeleteGroup()
+	Duel.Destroy(tg,REASON_EFFECT)
 end
