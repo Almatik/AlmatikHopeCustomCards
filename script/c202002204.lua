fecbc5fa3329b04510529fa62674c6a262dd8864
Almatik <almatiktk@gmail.com>
2021-12-05 17:30:18 +0600

    f

--- script/c202002204.lua
+++ script/c202002204.lua
@@ -13,9 +13,11 @@ function s.initial_effect(c)
 	--actlimit
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_SZONE,LOCATION_SZONE)
+	e2:SetTargetRange(0,1)
+	e2:SetValue(1)
 	e2:SetCondition(s.actcon)
 	c:RegisterEffect(e2)
 	--Substitute destruction for 1 "Shinigami" monster
@@ -32,29 +34,47 @@ function s.initial_effect(c)
 
 	
 end
-function s.filter(c)
+function s.filter1(c)
 	return c:IsCode(202002011) and c:IsAbleToHand()
+end
+function s.filter1(c,e,tp)
+	return c:IsCode(202002011) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local tc=Duel.GetFirstMatchingCard(s.filter,tp,LOCATION_DECK,0,nil)
-	if tc and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		Duel.SendtoHand(tc,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,tc)
+	local tc1=Duel.GetFirstMatchingCard(s.filter,tp,LOCATION_DECK,0,nil)
+	local tc2=Duel.GetFirstMatchingCard(s.filter,tp,LOCATION_HAND,0,nil,e,tp)
+	if tc1 or tc2 then
+		local sel={}
+		table.insert(sel,aux.Stringid(id,0))
+		table.insert(sel,aux.Stringid(id,1))
+		table.insert(sel,aux.Stringid(id,2))
+		local res=Duel.SelectOption(tp,false,table.unpack(sel))
+		if res==0 then return end
+		Duel.BreakEffect()
+		if res==1 then
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tc1)
+		end
+		if res==2 then
+			Duel.SpecialSummon(tc2,0,tp,tp,false,false,POS_FACEUP)
+		end
 	end
 end
 
 
 
 
-function s.actfilter(c)
-	return c:IsSetCard(0x2010)
+function s.cfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(0x2010) and c:IsControler(tp)
 end
-function s.actcon(e,tp)
-	local ph=Duel.GetCurrentPhase()
-	return Duel.IsExistingMatchingCard(s.actfilter,tp,LOCATION_MZONE,0,1,nil)
-	and ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
+function s.actcon(e)
+	local tp=e:GetHandlerPlayer()
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	return (a and s.cfilter(a,tp)) or (d and s.cfilter(d,tp))
 end
+
 
 
 
