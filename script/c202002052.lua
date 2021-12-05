b8615a0bbe3819fec060fb1a5d7cd5f7a9bc6982
Almatik <almatiktk@gmail.com>
2021-12-05 14:40:47 +0600

    f

--- script/c202002052.lua
+++ script/c202002052.lua
@@ -14,7 +14,19 @@ function s.initial_effect(c)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
-	
+	-- Negate
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_DISABLE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_HAND+LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.negcon)
+	e3:SetCost(s.negcost)
+	e3:SetTarget(s.negtg)
+	e3:SetOperation(s.negop)
+	c:RegisterEffect(e3)
 end
 function s.sfilter(c)
 	return c:IsSetCard(0x2010) and c:IsType(TYPE_MONSTER) and c:IsLevelAbove(7)
@@ -27,11 +39,34 @@ function s.sop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,s.sfilter,tp,LOCATION_DECK,0,1,1,nil)
 	local tc=g:GetFirst()
 	if tc and Duel.SendtoHand(tc,tp,REASON_EFFECT)~=0 and Duel.SelectYesNo(tp,aux.Stringid(id,1))~=0 then
-		if Duel.SendtoHand(c,tp,REASON_EFFECT)~=0 and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 then
-			local g=Duel.SelectMatchingCard(tp,Card.IsCode,tp,LOCATION_HAND,0,1,1,nil,202002011)
-			if #g>0 then
-				Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-			end
+		if Duel.SendtoHand(c,tp,REASON_EFFECT)~=0 and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 and tc:IsCode(202002011) then
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 		end
 	end
+end
+
+
+
+
+
+
+
+function s.negconfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x2010)
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp and re:IsActiveType(TYPE_MONSTER)
+		and Duel.IsExistingMatchingCard(s.negconfilter,tp,LOCATION_MZONE,0,1,nil,tp)
+end
+function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToGraveAsCost() end
+	Duel.SendtoGrave(c,REASON_COST)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsChainDisablable(ev) and not re:GetHandler():IsStatus(STATUS_DISABLED) end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateEffect(ev)
 end
