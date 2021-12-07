6223b154b62264ff3b9622f402e7968569f35638
Almatik <almatiktk@gmail.com>
2021-12-07 09:15:45 +0600

    Add files via upload

---
+++ script/c2021030031.lua
@@ -0,0 +1,30 @@
+--Ruddy Magician
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	c:EnableCounterPermit(COUNTER_SPELL)
+	c:SetCounterLimit(COUNTER_SPELL,c:GetLevel())
+	--add counter
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetCode(EVENT_CHAINING)
+	e0:SetRange(LOCATION_MZONE)
+	e0:SetOperation(aux.chainreg)
+	c:RegisterEffect(e0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e1:SetCode(EVENT_CHAIN_SOLVED)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetOperation(s.acop)
+	c:RegisterEffect(e1)
+	
+end
+s.counter_place_list={COUNTER_SPELL}
+s.listed_names={2021030032}
+function s.acop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:GetFlagEffect(1)>0 then
+		c:AddCounter(COUNTER_SPELL,1)
+	end
+end
