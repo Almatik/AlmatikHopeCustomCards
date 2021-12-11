d7d87a3a592583ce5c5b2ff4ffee97b8a778d43f
Almatik <almatiktk@gmail.com>
2021-12-11 23:43:18 +0600

    Update c2021020011.lua

--- script/c2021020011.lua
+++ script/c2021020011.lua
@@ -19,7 +19,8 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 
 	--Add Random Deck
 	local decknum=Duel.GetRandomNumber(1,1)
-	local deck=s.deck[decknum][1]+s.deck[decknum][2]
+	table.insert(s.deck[decknum][1],s.deck[decknum][2])
+	local deck=s.deck[decknum][1]
 	for code,codex in ipairs(deck) do
 		Debug.AddCard(codex,tp,tp,LOCATION_DECK,1,POS_FACEDOWN)
 	end
