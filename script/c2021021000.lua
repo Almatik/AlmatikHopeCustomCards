cf3e1173680a46e6ea81eb95f3867c55bc525a56
Almatik <almatiktk@gmail.com>
2021-12-12 02:58:12 +0600

    Update c2021021000.lua

--- script/c2021021000.lua
+++ script/c2021021000.lua
@@ -45,7 +45,9 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 
 
 	--Field
-	local token=Duel.CreateToken(tp,deckid)
+	--local token=Duel.CreateToken(tp,deckid)
+	Duel.Hint(HINT_SKILL_FLIP,tp,deckid|(1<<32))
+	local token=Duel.GetFieldGroup(tp,LOCATION_FZONE,0)
 	e:SetLabelObject(token)
 		 --redirect
 	local e1=Effect.CreateEffect(token)
