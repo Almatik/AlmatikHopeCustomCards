a208f4a023596845c05f2728525f045bb36b8929
Almatik <almatiktk@gmail.com>
2021-12-11 13:32:37 +0600

    а

--- script/c202002303.lua
+++ script/c202002303.lua
@@ -1,17 +1,9 @@
 --Hozukimaru the Winter Cherry
+Duel.LoadScript("bleach.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableUnsummonable()
-	--Search
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
-	c:RegisterEffect(e1)
+	Bleach.AddUnionProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x2010),true)
 	--Double Piercing
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_EQUIP)
