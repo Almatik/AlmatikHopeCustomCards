54013b0c91873671536dcea68b3785c74bcebba8
Almatik <almatiktk@gmail.com>
2021-12-11 14:39:07 +0600

    Update c2021020011.lua

--- script/c2021020011.lua
+++ script/c2021020011.lua
@@ -2,19 +2,28 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--skill
-	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
+	local e1=Effect.CreateEffect(c) 
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetRange(0x5f)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
 end
-function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--condition
-	return true
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.SendtoDeck(c,tp,-2,REASON_RULE)
+	local g=Duel.GetFieldGroup(tp,LOCATION_ALL,0)
+	Duel.SendtoDeck(g,tp,-2,REASON_RULE)
+	local decknum=Duel.GetRandomNumber(1,#s.deck)
+	local deck=s.deck[decknum]
+	for code in s.deck[decknum] do
+		Debug.AddCard(code,tp,tp,LOCATION_DECK,1,POS_FACEDOWN_DEFENSE)
+	end
 end
-function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	--ask if you want to activate the skill or not
-	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
-	Duel.Hint(HINT_CARD,tp,id)
-	local sel1={aux.Stringid(id,0)}
-	local sel2={aux.Stringid(id,1)}
-	Duel.SelectOption(tp,false,table.unpack(sel1))
-	Duel.SelectOption(tp,false,table.unpack(sel2))
-	aux.RegisterClientHint(e:GetHandler(),nil,1-tp,1,0,aux.Stringid(id,2),nil)
-end
+
+s.deck={}
+	s.deck[1]={22073844,68468459}
+	s.deck[2]={62941499,45484331}
+	s.deck[3]={47710198,82489470}
+	s.deck[4]={99726621,19096726}
