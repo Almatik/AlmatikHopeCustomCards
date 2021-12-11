5cefd085ac89170cd60b70bb7e46c03541450e2d
Almatik <almatiktk@gmail.com>
2021-12-11 15:19:01 +0600

    Update c2021020011.lua

--- script/c2021020011.lua
+++ script/c2021020011.lua
@@ -16,8 +16,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,LOCATION_ALL,0)
 	Duel.SendtoDeck(g,tp,-2,REASON_RULE)
 	local decknum=Duel.GetRandomNumber(1,#s.deck)
-	local code=s.deck[decknum]
-	for idx,code in aux.Next(s.deck[decknum]) do
+	for code in aux.Next(s.deck[decknum]) do
 		Debug.AddCard(code,tp,tp,LOCATION_DECK,1,POS_FACEDOWN_DEFENSE)
 	end
 	Duel.ShuffleDeck(tp)
