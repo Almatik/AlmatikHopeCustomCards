57d44e149d1ab7cd59a0395670a060871d7b7179
Almatik <almatiktk@gmail.com>
2021-12-12 01:53:39 +0600

    Update c2021021000.lua

--- script/c2021021000.lua
+++ script/c2021021000.lua
@@ -28,8 +28,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		for _,v in ipairs(s.deck) do
 			table.insert(decklist,id+1+#decklist)
 		end
-		local deckcode=Duel.SelectCardsFromCodes(tp,1,1,false,false,table.unpack(decklist))
-		local decknum=deckcode:GetCode()-id
+		local decknum=Duel.SelectCardsFromCodes(tp,1,1,false,false,table.unpack(decklist)):GetCode()
 	end
 	local deck=s.deck[decknum][1]
 	local extra=s.deck[decknum][2]
