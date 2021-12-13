c37cfd89596372a0f3d9d61287107c4f6d763af6
Almatik <almatiktk@gmail.com>
2021-12-13 23:00:02 +0600

    Update c2021021000.lua

--- script/c2021021000.lua
+++ script/c2021021000.lua
@@ -24,7 +24,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		deckid=decknum+id
 	else
 		local up=table.unpack(s.deck)
-		deckid=Duel.SelectCardsFromCodes(tp,1,1,false,false,table.unpack(up[3]))
+		deckid=Duel.SelectCardsFromCodes(tp,1,1,false,false,table.unpack(s.deck[up][3]))
 		decknum=deckid-id
 	end
 	--Add Random Deck
