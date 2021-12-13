c92a4b5da81bbd0d343bb9c73158b1611f18af78
Almatik <almatiktk@gmail.com>
2021-12-13 20:31:02 +0600

    Update c2021021000.lua

--- script/c2021021000.lua
+++ script/c2021021000.lua
@@ -19,7 +19,6 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	table.insert(sel,aux.Stringid(id,0))
 	table.insert(sel,aux.Stringid(id,1))
 	local selop=Duel.SelectOption(tp,false,table.unpack(sel))
-	local g
 	if selop==0 then
 
 		--Add Random Deck
@@ -37,7 +36,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 
 
 		--Add Covers
-		local g=Duel.GetFieldGroup(tp,LOCATION_ALL,0)
+		g=Duel.GetFieldGroup(tp,LOCATION_ALL,0)
 		local tc=g:GetFirst()
 		while tc do
 			--generate a cover for a card
