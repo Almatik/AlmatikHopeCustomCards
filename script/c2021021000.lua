26501e63ad21bdbf9e83bf5185ad3c07aa4bde2a
Almatik <almatiktk@gmail.com>
2021-12-13 20:46:52 +0600

    Update c2021021000.lua

--- script/c2021021000.lua
+++ script/c2021021000.lua
@@ -22,8 +22,8 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if selop==0 then
 		decknum=Duel.GetRandomNumber(1,#s.deck)+id
 	else
-		local codelist=table.unpack(#s.deck)
-		decknum=Duel.SelectCardsFromCodes(tp,1,1,false,false,codelist[3])
+		local codelist=table.unpack(#s.deckcode)
+		decknum=Duel.SelectCardsFromCodes(tp,1,1,false,false,codelist)
 	end
 	--Add Random Deck
 	local deckid=decknum
@@ -64,7 +64,7 @@ end
 
 
 s.deck={}
-s.deckcover={}
+s.deckcode={}
 	--"Albuz Dogmatik"
 	s.deck[1]={}
 			--Main Deck
@@ -72,8 +72,7 @@ s.deckcover={}
 			--Extra Deck
 	s.deck[1][2]={44146295,44146295,34848821,41373230,41373230,41373230,87746184,87746184,80532587,80532587,80532587,79606837,79606837,79606837,70369116}
 			--Deck Cover
-	s.deck[1][3]=2021021001
-	s.deckcover[1]=2021021001
+	s.deckcode[1]=2021021001
 
 
 	--"Albuz Springans"
