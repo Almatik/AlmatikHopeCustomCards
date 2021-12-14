77e3fce660e1a893ac41f029d6fba8bf48579286
Almatik <almatiktk@gmail.com>
2021-12-14 22:02:42 +0600

    Update c2021021000.lua

--- script/c2021021000.lua
+++ script/c2021021000.lua
@@ -93,7 +93,6 @@ end
 
 
 function s.relaymode(c,tp)
-	local tp=e:GetHandlerPlayer()
 	local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
