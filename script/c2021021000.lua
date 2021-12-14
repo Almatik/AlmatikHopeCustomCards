6d69076488afd213e3c6f12237ff11b1d373a5fc
Almatik <almatiktk@gmail.com>
2021-12-14 21:02:42 +0600

    Update c2021021000.lua

--- script/c2021021000.lua
+++ script/c2021021000.lua
@@ -53,6 +53,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 
 	startlp=Duel.GetLP(tp)
 	--Relay 1v1 (3 Deck) Mode
+	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
