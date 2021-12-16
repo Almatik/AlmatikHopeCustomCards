110820fff23f78b72233957ce6dee4af650f9481
Almatik <almatiktk@gmail.com>
2021-12-16 15:38:21 +0600

    Update c2021030040.lua

--- script/c2021030040.lua
+++ script/c2021030040.lua
@@ -33,9 +33,10 @@ end
 function s.operations(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	local c=e:GetHandler()
+	if not tc or tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc)
-	local sc=g:GetFirst()
+	local g=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,e,tp,tc)
+	local sc=g:Select(tp,1,1,nil):GetFirst()
 	if sc then
 		sc:SetMaterial(tc)
 		Duel.Overlay(sc,tc)
