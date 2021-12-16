b7b950455711324ea76018dd12b3840bc09c54ef
Almatik <almatiktk@gmail.com>
2021-12-16 13:03:41 +0600

    Update c2021030040.lua

--- script/c2021030040.lua
+++ script/c2021030040.lua
@@ -13,56 +13,39 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-function s.filter1(c,tp,mc)
+function s.filter1(c,e,tp)
 	return c:IsFaceup()
-		and (c:IsType(TYPE_XYZ) or c:HasLevel())
-		 and Duel.IsExistingTarget(s.filter2,tp,LOCATION_EXTRA,0,1,nil,tp,mc,c)
+		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,c)
 end
-function s.filter2(c,tp,mc,sc)
-	if sc:IsType(TYPE_XYZ) then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_XYZ_LEVEL)
-		e1:SetValue(sc:GetRank())
-		e1:SetReset(RESET_CHAIN)
-		sc:RegisterEffect(e1)
-	end
-	local mg=Group.FromCards(mc,sc)
-	local chk=c:IsXyzSummonable(nil,mg,2,2)
-	e1:Reset()
-	return chk and Duel.GetLocationCountFromEx(tp,tp,mg,c)>0
+function s.filter2(c,e,tp,mc
+	if c.rum_limit and not c.rum_limit(mc,e) then return false end
+	if mc:IsType(XYZ) then lv=mc:GetRank() elseif mc:HasLevel() then lv=mc:GetLevel()
+	return mc:IsType(TYPE_XYZ,c,SUMMON_TYPE_XYZ,tp) and c:IsRank(lv) and mc:IsCanBeXyzMaterial(c,tp)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
+		and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
 end
-function s.target(e,tp,eg,ev,ep,re,r,rp,chk,chkc)
-	local c=e:GetHandler()
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter1(chkc,tp,c) end
-	if chk==0 then return Duel.IsPlayerCanSpecialSummonCount(tp,2)
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil,tp,c) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-	Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil,e,tp,c)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter1(chkc,e,tp) end
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function s.operation(e,tp,eg,ev,ep,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local mc=e:GetHandler()
-	local sc=Duel.GetFirstTarget()
-	if not mc:IsRelateToEffect(e) or Duel.SpecialSummon(mc,0,tp,tp,false,false,POS_FACEUP)==0 then return end
-	if not sc:IsRelateToEffect(e) or sc:IsFacedown() then return end
-	local mg=Group.FromCards(mc,sc)
-	local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,tp,mc,sc)
-	if #g>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=g:Select(tp,1,1,nil)
-		if tc:IsType(TYPE_XYZ) then
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_XYZ_LEVEL)
-			e1:SetValue(sc:GetRank())
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			xc:RegisterEffect(e1)
+function s.operations(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local c=e:GetHandler()
+	if not tc or tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc)
+	local sc=g:GetFirst()
+	if sc then
+		sc:SetMaterial(tc)
+		Duel.Overlay(sc,tc)
+		Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
+		sc:CompleteProcedure()
+		if c:IsRelateToEffect(e) then
+			Duel.Overlay(sc,c)
 		end
-		Duel.XyzSummon(tp,sg:GetFirst(),mc,mg)
 	end
 end
+
