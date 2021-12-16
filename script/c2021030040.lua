702c933188ad5c04d067c51b41d766adba65448f
Almatik <almatiktk@gmail.com>
2021-12-16 16:22:40 +0600

    f

--- script/c2021030040.lua
+++ script/c2021030040.lua
@@ -23,7 +23,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function s.xyzlv(e,c,rc)
-	if rc:IsSetCard(0x4001) then
+	if rc:IsSetCard(0x4002) then
 		return 1,2,3,4,5,6,7,8,9,10,11,2
 	else
 		return e:GetHandler():GetLevel()
