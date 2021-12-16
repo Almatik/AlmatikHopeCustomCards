53d0cd968f664ed347ab4fe6fa01346f6ce47e8d
Almatik <almatiktk@gmail.com>
2021-12-16 15:55:18 +0600

    Update c2021030040.lua

--- script/c2021030040.lua
+++ script/c2021030040.lua
@@ -30,9 +30,9 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function s.operations(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
 	if not tc or tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc)
