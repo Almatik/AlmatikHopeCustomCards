81dfe32cc76b957a67898d2150cb40cac6cbad53
Almatik <almatiktk@gmail.com>
2021-12-19 20:51:20 +0600

    Update c2021020011.lua

--- script/c2021020011.lua
+++ script/c2021020011.lua
@@ -2,6 +2,18 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
+	local e1=Effect.CreateEffect(c) 
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PREDRAW)
+	e1:SetRange(0x5f)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsLocation()==LOCATION_HAND then Duel.Draw(tp,1,REASON_RULE) end
+	Duel.SendtoDeck(c,tp,-2,REASON_RULE)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
@@ -12,4 +24,5 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 	Duel.TagSwap(tp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
 end
