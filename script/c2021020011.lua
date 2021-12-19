7d3c2f9878cb61de1dc66969fec99e5b47c22ab1
Almatik <almatiktk@gmail.com>
2021-12-19 20:44:26 +0600

    Update c2021020011.lua

--- script/c2021020011.lua
+++ script/c2021020011.lua
@@ -2,14 +2,10 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	aux.AddSkillProcedure(c,2,false,nil,nil)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetRange(0x5f)
-	e1:SetOperation(s.tag)
-	c:RegisterEffect(e1)
+	aux.AddSkillProcedure(c,1,false,nil,s.flipop,1)
 end
-function s.tag(e,tp,eg,ep,ev,re,r,rp)
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
 	Duel.TagSwap(tp)
 end
