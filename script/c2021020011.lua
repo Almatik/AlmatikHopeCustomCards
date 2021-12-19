af6f38f1c6fccb709acebdb936fef6fbb2f504ea
Almatik <almatiktk@gmail.com>
2021-12-19 20:45:09 +0600

    Update c2021020011.lua

--- script/c2021020011.lua
+++ script/c2021020011.lua
@@ -2,7 +2,11 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	aux.AddSkillProcedure(c,1,false,nil,s.flipop,1)
+	aux.AddSkillProcedure(c,1,false,flipcon,s.flipop,1)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return aux.CanActivateSkill(tp)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
