d927a0f68cf23cfb12f68d5e0f8e69283d97a007
Almatik <almatiktk@gmail.com>
2021-12-19 20:39:56 +0600

    Update c2021020011.lua

--- script/c2021020011.lua
+++ script/c2021020011.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	aux.AddSkillProcedure(c,2,false,nil,nil)
-	local e1=Effect.CreateEffect()
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(0x5f)
