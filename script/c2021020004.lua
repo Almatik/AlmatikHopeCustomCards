4587e068c5596cb50f284f12ede7a0a628fbabe6
Almatik <almatiktk@gmail.com>
2021-12-23 15:30:49 +0600

    Add files via upload

--- script/c2021020004.lua
+++ script/c2021020004.lua
@@ -12,7 +12,6 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.Dwheel={2021020005,2021020006,2021020007,2021020008,2021020009}
-s.RidingSpeed=nil
 function s.op(e,tp,eg,ep,ev,re,r,rp)
 	Duel.DisableShuffleCheck()
 	Duel.SendtoDeck(e:GetHandler(),tp,-2,REASON_RULE)
@@ -37,6 +36,15 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetLabelObject(tc)
 	e2:SetOperation(s.ReturnField)
 	Duel.RegisterEffect(e2,tp)
+	aux.GlobalCheck(s,function()
+		s[0]=nil
+		s[1]=nil
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_ADJUST)
+		ge1:SetOperation(RidingDuel.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end)
 	--AddCounter
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -47,14 +55,14 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	tc:RegisterEffect(e3)
 end
 function s.checkop(tp)
-	if not s.RidingSpeed or s.RidingSpeed~=Duel.GetCounter(tp,LOCATION_FZONE,0,0x91) then
-		s.RidingSpeed=Duel.GetCounter(tp,LOCATION_FZONE,0,0x91)
+	if not s[tp] or s[tp]~=Duel.GetCounter(tp,LOCATION_FZONE,0,0x91) then
+		s[tp]=Duel.GetCounter(tp,LOCATION_FZONE,0,0x91)
 	end
 	local tc=Duel.GetFirstMatchingCard(Card.IsFaceup,tp,LOCATION_FZONE,0,nil)
 	if tc:GetCounter(0x91)==0 or not tc:GetCounter(0x91) then
 		tc:EnableCounterPermit(0x91)
 		tc:SetCounterLimit(0x91,12)
-		tc:AddCounter(0x91,s.RidingSpeed)
+		tc:AddCounter(0x91,s[tp])
 	end
 end
 function s.RemoveField(e,tp)
