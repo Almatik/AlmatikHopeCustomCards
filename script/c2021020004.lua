b19f9b040958ede4aa324269d400bd36b3c3314a
Almatik <almatiktk@gmail.com>
2021-12-23 15:48:37 +0600

    Add files via upload

--- script/c2021020004.lua
+++ script/c2021020004.lua
@@ -60,7 +60,7 @@ function s.checkop(tp)
 	if tc:GetFlagEffect(id)==0 then
 		tc:EnableCounterPermit(0x91)
 		tc:SetCounterLimit(0x91,12)
-		tc:AddCounter(0x91,s[tp])
+		if s[tp]~=0 then tc:AddCounter(0x91,s[tp]) end
 		tc:RegisterFlagEffect(id,0,0,0)
 	end
 end
