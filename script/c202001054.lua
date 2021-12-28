581323f484656e2f43e52733b7481ca53aad2da2
Almatik <almatiktk@gmail.com>
2021-12-28 16:51:43 +0600

    Add files via upload

--- script/c202001054.lua
+++ script/c202001054.lua
@@ -18,10 +18,179 @@ function s.initial_effect(c)
 	local e3=e2:Clone()
 	e3:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e3)
+	--spsummon
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetCountLimit(1,{id,1})
+	e4:SetCondition(s.con1)
+	e4:SetTarget(s.tg1)
+	e4:SetOperation(s.op1)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetCountLimit(1,{id,2})
+	e5:SetCondition(s.con2)
+	e5:SetTarget(s.tg2)
+	e5:SetOperation(s.op2)
+	c:RegisterEffect(e5)
+	local e6=e4:Clone()
+	e6:SetDescription(aux.Stringid(id,3))
+	e6:SetCountLimit(1,{id,3})
+	e6:SetCondition(s.con3)
+	e6:SetTarget(s.tg3)
+	e6:SetOperation(s.op3)
+	c:RegisterEffect(e6)
+	local e7=e4:Clone()
+	e7:SetDescription(aux.Stringid(id,4))
+	e7:SetCountLimit(1,{id,4})
+	e7:SetCondition(s.con4)
+	e7:SetTarget(s.tg4)
+	e7:SetOperation(s.op4)
+	c:RegisterEffect(e7)
+	local e8=e4:Clone()
+	e8:SetDescription(aux.Stringid(id,5))
+	e8:SetCountLimit(1,{id,5})
+	e8:SetCondition(s.con5)
+	e8:SetTarget(s.tg5)
+	e8:SetOperation(s.op5)
+	c:RegisterEffect(e8)
 end
 function s.filter(c)
 	return c:IsFaceup() and c:IsLinked()
 end
 function s.value(e,c)
 	return Duel.GetMatchingGroup(s.filter,c:GetControler(),LOCATION_MZONE,LOCATION_MZONE,nil):GetCount()*100
+end
+
+
+
+--"Karakura"
+function s.cfilter1(c,tp,re)
+	return c:IsPreviousControler(1-tp) and c:IsPreviousLocation(LOCATION_MZONE) and c:GetReasonPlayer()==tp and re:GetHandler():IsSetCard(0x2000)
+end
+function s.con1(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter1,1,nil,tp,re)
+end
+function s.tg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.op1(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
+
+
+
+--"Shinigami"
+function s.cfilter2(c,tp,re)
+	return c:IsPreviousControler(1-tp) and c:IsPreviousLocation(LOCATION_MZONE) and c:GetReasonPlayer()==tp and re:GetHandler():IsSetCard(0x2010)
+end
+function s.con2(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter2,1,nil,tp,re)
+end
+function s.tg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+end
+function s.op2(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g,true)
+		Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	end
+end
+
+
+
+--"Quincy"
+function s.cfilter3(c,tp,re)
+	return c:IsPreviousControler(1-tp) and c:IsPreviousLocation(LOCATION_MZONE) and c:GetReasonPlayer()==tp and re:GetHandler():IsSetCard(0x2040)
+end
+function s.con3(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter3,1,nil,tp,re)
+end
+function s.tg3(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOV,nil,1,tp,LOCATION_GRAVE)
+end
+function s.op3(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g,true)
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	end
+end
+
+
+
+--"Hollow"
+function s.cfilter4(c,tp,re)
+	return c:IsPreviousControler(1-tp) and c:IsPreviousLocation(LOCATION_MZONE) and c:GetReasonPlayer()==tp and re:GetHandler():IsSetCard(0x2020)
+end
+function s.con4(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter4,1,nil,tp,re)
+end
+function s.tg4(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(Card.IsCanBeSpecialSummoned,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,e,0,tp,false,false,POS_FACEUP_DEFENSE) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.op4(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,Card.IsCanBeSpecialSummoned,tp,LOCATION_GRAVE,0,1,1,nil,e,0,tp,false,false,POS_FACEUP_DEFENSE)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		tc:RegisterEffect(e2)
+		local e3=e1:Clone()
+		e3:SetCode(EFFECT_SET_ATTACK)
+		e3:SetValue(0)
+		tc:RegisterEffect(e3)
+		local e4=e1:Clone()
+		e4:SetCode(EFFECT_SET_DEFENSE)
+		e4:SetValue(0)
+		tc:RegisterEffect(e4)
+	end
+end
+
+
+
+--"Xcution"
+function s.cfilter5(c,tp,re)
+	return c:IsPreviousControler(1-tp) and c:IsPreviousLocation(LOCATION_MZONE) and c:GetReasonPlayer()==tp and re:GetHandler():IsSetCard(0x2030)
+end
+function s.con5(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter5,1,nil,tp,re)
+end
+function s.tg5(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(Card.IsCanBeSpecialSummoned,tp,LOCATION_HAND,0,1,nil,e,0,tp,false,false,POS_FACEUP_DEFENSE) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.op5(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,Card.IsCanBeSpecialSummoned,tp,LOCATION_HAND,0,1,1,nil,e,0,tp,false,false,POS_FACEUP_DEFENSE)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
 end
