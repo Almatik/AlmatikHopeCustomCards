4feb14c6e4ebc26b3f88225349a7611c71650a6b
Almatik <almatiktk@gmail.com>
2021-12-28 11:07:20 +0600

    Add files via upload

---
+++ script/c202001054.lua
@@ -0,0 +1,27 @@
+--Karakura Town
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--atk/def up
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsLinked))
+	e2:SetValue(s.value)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e3)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsLinked()
+end
+function s.value(e,c)
+	return Duel.GetMatchingGroup(s.filter,c:GetControler(),LOCATION_MZONE,LOCATION_MZONE,nil):GetCount()*100
+end
