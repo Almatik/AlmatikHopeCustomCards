7328d02c54af92bae0ee34decf49855bbd061dcf
Almatik <almatiktk@gmail.com>
2022-02-28 13:12:51 +0600

    Add files via upload

--- script/c1000000001.lua
+++ script/c1000000001.lua
@@ -6,6 +6,7 @@ function s.initial_effect(c)
 	Xyz.AddProcedure(c,s.xyzfilter,nil,2,nil,nil,nil,nil,false)
 	--Unnafected by other cards' effects
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_IMMUNE_EFFECT)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -15,6 +16,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--atk/def up
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
 	e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
 	e2:SetType(EFFECT_TYPE_QUICK_F)
 	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
@@ -22,13 +24,27 @@ function s.initial_effect(c)
 	e2:SetCondition(s.battlecon)
 	e2:SetOperation(s.battleop)
 	c:RegisterEffect(e2)
+	--Special Summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,3))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetCode(EVENT_BATTLE_DESTROYING)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCountLimit(1,{id,3})
+	e3:SetCondition(aux.bdogcon)
+	e3:SetCost(aux.dxmcostgen(1,1,nil))
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
 end
 s.listed_series={0x107f}
+
+	--2 Rank 4 "Utopia" Xyz Monsters
 function s.xyzfilter(c,xyz,sumtype,tp)
 	return c:IsType(TYPE_XYZ,xyz,sumtype,tp) and c:IsRank(4) and c:IsSetCard(0x107f,xyz,sumtype,tp)
 end
 
---atk/def up
+	--atk/def up
 function s.unecon(e)
 	return e:GetHandler():GetOverlayCount()>0
 end
@@ -58,5 +74,22 @@ function s.battleop(e,tp,eg,ep,ev,re,r,rp)
 			e2:SetValue(atk*500)
 			c:RegisterEffect(e2)
 		end
+	end
+end
+
+
+
+	--Special Summon
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local bc=e:GetHandler():GetBattleTarget()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and bc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP) end
+	Duel.SetTargetCard(bc)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,bc,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
