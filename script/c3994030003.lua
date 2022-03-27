f6881b2f35ef5bee5be7f37e820045988bc92a5b
Almatik <almatiktk@gmail.com>
2022-03-27 22:08:29 +0600

    Create c3994030003.lua

---
+++ script/c3994030003.lua
@@ -0,0 +1,15 @@
+--Road Magic - Dark Night
+local s,id=GetID()
+function s.initial_effect(c)
+	--Change the positions of up to 3 opponent's monsters
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	
+end
