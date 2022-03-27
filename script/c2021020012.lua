08c1ce8ba1d8c8401e2c32c8b425c123794c7615
Almatik <almatiktk@gmail.com>
2022-03-27 23:49:57 +0600

    Update c2021020012.lua

--- script/c2021020012.lua
+++ script/c2021020012.lua
@@ -14,7 +14,8 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_DECK,0,nil,TYPE_MONSTER+TYPE_NORMAL)
 	local tc=g:GetFirst()
 	for tc in aux.Next(g) do
-		s.EffectList[1]
+		local effnum=Duel.GetRandomNumber(1,#s.EffectList)
+		s.EffectList[effnum]
 	end
 end
 
@@ -27,10 +28,10 @@ end
 s.EffectList={}
 
 s.EffectList[1]={
-					local e1=Effect.CreateEffect(e:GetHandler())
-					e1:SetType(EFFECT_TYPE_SINGLE)
-					e1:SetCode(EFFECT_UPDATE_ATTACK)
-					e1:SetValue(100)
-					e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-					tc:RegisterEffect(e1)
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_UPDATE_ATTACK)
+				e1:SetValue(100)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+				tc:RegisterEffect(e1)
 				}
