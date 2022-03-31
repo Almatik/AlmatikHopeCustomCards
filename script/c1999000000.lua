65f0628fe44daad44ab3a2c87a1bddc78e954104
Almatik <almatiktk@gmail.com>
2022-03-31 20:47:23 +0600

    Update c1999000000.lua

--- script/c1999000000.lua
+++ script/c1999000000.lua
@@ -61,7 +61,7 @@ function s.PackOpen(e,tp,format,series,pack)
 	local cpp=s.Pack[format][series][pack][10]
 	for i=1,cpp do
 		local rarity=1
-		if i=cpp then
+		if i==cpp then
 			local chance=Duel.GetRandomNumber(1,100)
 			if chance>16 and #s.Pack[format][series][pack][2]>0 then rarity=2
 				elseif chance>8 and #s.Pack[format][series][pack][3]>0 then rarity=3
