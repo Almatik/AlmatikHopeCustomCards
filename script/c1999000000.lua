14d86bebc82d46e55035d12f185d3d03d9eb34dc
Almatik <almatiktk@gmail.com>
2022-03-31 22:12:28 +0600

    Update c1999000000.lua

--- script/c1999000000.lua
+++ script/c1999000000.lua
@@ -127,9 +127,9 @@ function s.AutoDeckMode(e,tp,format,series)
 			e1:SetValue(0)
 			tc:RegisterEffect(e1)
 		end
-		local add=Duel.GetFieldGroup(tp,LOCATION_HAND,0):Select(tp,0,1,nil)
-		local del=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
-		Duel.SendtoDeck(del,tp,1,REASON_RULE)
+		local add=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
+		add:Select(tp,0,1,nil)
+		Duel.SendtoDeck(add,tp,1,REASON_RULE)
 	end
 	local del=Duel.GetFieldGroup(tp,LOCATION_GRAVE,0)
 	Duel.SendtoDeck(del,tp,-2,REASON_RULE)
