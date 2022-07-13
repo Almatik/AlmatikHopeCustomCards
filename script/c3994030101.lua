7e8e88243cd962a278665445649122539814857a
Almatik <almatiktk@gmail.com>
2022-07-13 19:53:21 +0600

    f

--- script/c3994030101.lua
+++ script/c3994030101.lua
@@ -1,101 +1,35 @@
 --Spidærk Elise
 local s,id=GetID()
 function s.initial_effect(c)
-	--Other material is treated as a Level 1
+	--① If you control face-up "Spidærk Web" or "Shadow Isles" card, you can Special Summon this card (from your hand).
 	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_SYNCHRO_CHECK)
-	e1:SetValue(s.syncheck)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetCondition(s.spcon)
 	c:RegisterEffect(e1)
-	--Synchro Summon
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
-	e2:SetCountLimit(1,id)
-	e2:SetCondition(s.spcon)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
-	c:RegisterEffect(e2)
 end
 --Shadow Isles, Spidærk
 s.listed_series={0x39d6,0x39e0}
+--Spidærk Web
+s.listed_name={3994030100}
 
 
 
-	--Other material is treated as a Level 1
-function s.syncheck(e,c)
-	c:AssumeProperty(ASSUME_LEVEL,1)
-	return true
-end
 
 
 
-	--Synchro Summon
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp
+
+
+
+	--① If you control face-up "Spidærk Web" or "Shadow Isles" card, you can Special Summon this card (from your hand).
+function s.spfilter(c)
+	return c:IsFaceup()
+		and (c:IsCode(3994030100) or c:IsSetCard(0x39d6))
 end
-function s.filter(tc,c,tp)
-	if not tc:IsFaceup() or not tc:IsCanBeSynchroMaterial() then return false end
-	c:RegisterFlagEffect(id,0,0,1)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_SYNCHRO_MATERIAL)
-	tc:RegisterEffect(e1,true)
-	local mg=Group.FromCards(c,tc)
-	local res=Duel.IsExistingMatchingCard(s.synfilter,tp,LOCATION_EXTRA,0,1,nil,mg)
-	c:ResetFlagEffect(id)
-	e1:Reset()
-	return res
-end
-function s.synfilter(c,mg)
-	return c:IsRace(RACE_INSECT) and c:IsSynchroSummonable(nil,mg)
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.filter(chkc,e:GetHandler(),tp) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil,e:GetHandler(),tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil,e:GetHandler(),tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if c:IsRelateToEffect(e) and c:IsFaceup() and tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
-		c:RegisterFlagEffect(id,0,0,1)
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_SYNCHRO_MATERIAL)
-		tc:RegisterEffect(e1,true)
-		local mg=Group.FromCards(c,tc)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.synfilter,tp,LOCATION_EXTRA,0,1,1,nil,mg)
-		local sc=g:GetFirst()
-		if sc then
-			local e3=Effect.CreateEffect(c)
-			e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-			e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-			e3:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
-			e3:SetOperation(s.regop)
-			e3:SetLabelObject(e1)
-			sc:RegisterEffect(e3,true)
-			local e4=e3:Clone()
-			e4:SetCode(EVENT_SPSUMMON_NEGATED)
-			sc:RegisterEffect(e4,true)
-			Duel.SynchroSummon(tp,sc,nil,mg)
-		else
-			c:ResetFlagEffect(id)
-			e1:Reset()
-		end
-	end
-end
-function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	local rc=e:GetOwner()
-	local c=e:GetHandler()
-	rc:ResetFlagEffect(id)
-	e:GetLabelObject():Reset()
-	e:Reset()
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,c:GetControler(),LOCATION_ONFIELD,0,1,nil)
 end
